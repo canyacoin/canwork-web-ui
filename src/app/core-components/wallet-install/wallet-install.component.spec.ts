4dc859f0356cbd0714d2e1499c237adb4ba49d7e
Alex Scott <0AlexScott@gmail.com>
2018-06-19 12:32:51 +1000

    Made wallet instructions generic, added trust and solidifed provider setup process

---
+++ src/app/core-components/wallet-install/wallet-install.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WalletInstallComponent } from './wallet-install.component';
+
+describe('WalletInstallComponent', () => {
+  let component: WalletInstallComponent;
+  let fixture: ComponentFixture<WalletInstallComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ WalletInstallComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WalletInstallComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/exchange/exchange.module.ts
+++ src/app/exchange/exchange.module.ts
@@ -5,20 +5,15 @@ import { CoreComponentsModule } from '../core-components/core-components.module'
 import { CoreServicesModule } from '../core-services/core-services.module';
 import { ExchangeComponent } from './exchange.component';
 import { ExchangeRoutingModule } from './exchange.routing.module';
-import { FaqComponent } from './faq/faq.component';
-import { InstructionsComponent } from './instructions/instructions.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CoreComponentsModule,
-    CoreComponentsModule,
     ExchangeRoutingModule
   ],
   declarations: [
-    ExchangeComponent,
-    InstructionsComponent,
-    FaqComponent
+    ExchangeComponent
   ],
   exports: [
     ExchangeRoutingModule
