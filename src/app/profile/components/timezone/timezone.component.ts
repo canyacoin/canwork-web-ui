5a1fe7d08fdf0605b6f31b6d6b301b6147e7b7c7
Alex Scott <0AlexScott@gmail.com>
2018-06-21 11:16:29 +1000

    Fixed bug with profile editing

--- src/app/profile/components/timezone/timezone.component.ts
+++ src/app/profile/components/timezone/timezone.component.ts
@@ -3,6 +3,8 @@ import { Observable } from 'rxjs/Observable';
 import { interval } from 'rxjs/observable/interval';
 import { Subscription } from 'rxjs/Subscription';
 
+import { User, UserType } from '../../../core-classes/user';
+
 @Component({
   selector: 'app-profile-timezone',
   templateUrl: './timezone.component.html',
@@ -10,7 +12,7 @@ import { Subscription } from 'rxjs/Subscription';
 })
 export class TimezoneComponent implements OnInit, OnDestroy {
 
-  @Input() userModel: any;
+  @Input() userModel: User;
 
   myInterval = interval(1000);
   localTime: string;
@@ -23,6 +25,13 @@ export class TimezoneComponent implements OnInit, OnDestroy {
     this.timeSub = this.myInterval.subscribe(x => {
       this.localTime = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
     });
+  }
+
+  isProvider() {
+    if (this.userModel == null) {
+      return false;
+    }
+    return this.userModel.type === UserType.provider;
   }
 
   ngOnDestroy() {
--- src/app/profile/edit/currency.validator.ts
+++ src/app/profile/edit/currency.validator.ts
@@ -3,9 +3,9 @@ import { FormControl } from '@angular/forms';
 export class CurrencyValidator {
   static isValid(control: FormControl) {
     const re: any = /^\d{1,3}(?:\.\d{1,2})?$/.test(control.value);
-    if (re) {
+    if (re || control.value === '') {
       return null;
     }
-    return { 'invalidEmail': true };
+    return { 'invalidCurrency': true };
   }
 }
