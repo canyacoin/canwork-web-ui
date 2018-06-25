e79ee8a07b58f644810e6d7fe01c1d8741cf98d9
Alex Scott <0AlexScott@gmail.com>
2018-06-25 13:54:12 +1000

    Hotfixed query updates from homepage to homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
@@ -32,6 +32,8 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   providerSub: Subscription;
   portfolioSub: Subscription;
 
+  rendering = false;
+
   algoliaSearchConfig = {
     ...environment.algolia,
     indexName: environment.algolia.indexName,
@@ -39,9 +41,15 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   };
 
   constructor(private activatedRoute: ActivatedRoute, private navService: NavService,
-    private afs: AngularFirestore, private http: Http) {
-    this.routeSub = this.activatedRoute.params.subscribe((params) => {
+    private afs: AngularFirestore, private http: Http, private cdRef: ChangeDetectorRef) {
+    this.routeSub = this.activatedRoute.queryParams.subscribe((params) => {
       this.query = params['query'] ? params['query'] : '';
+      if (!this.loading) {
+        this.rendering = true;
+        setTimeout(() => {
+          this.rendering = false;
+        });
+      }
     });
   }
 
