be775b86c6002c865653b255625c4ec154b9682d
Alex Scott <0AlexScott@gmail.com>
2018-06-26 12:45:30 +1000

    Added generic footer

--- src/app/core-components/footer/footer.component.ts
+++ src/app/core-components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var createFooter: any;
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -10,6 +11,7 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    createFooter();
   }
 
 }
