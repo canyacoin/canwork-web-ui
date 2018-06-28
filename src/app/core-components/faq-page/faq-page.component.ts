a9ca7d5b41a3558ac4639215e4801905fd8b5d69
wie cahya <wie@canya.com>
2018-06-28 13:40:05 +1000

    Basic groundwork for FAQ

--- src/app/core-components/faq-page/faq-page.component.ts
+++ src/app/core-components/faq-page/faq-page.component.ts
@@ -6,7 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq-page.component.css']
 })
 export class FaqPageComponent implements OnInit {
+  faqs = [
+      {q: "Who is CanWork for?", a: "CANWork is for digital online services (eg. graphics designers, content writers, digital marketing specialists) Future service, CANTasker, will serve local services"},
+      {q: "Why CanWork?", a:""},
+      {q: "How do I get started?",a:""},
+      {q: "Do I have to pay to join?",a:"No, it's completely free!"},
+      {q: "Who is a 'provider'?",a:"A 'provider' is anyone offerring a service via the CanYa Ecosystem"},
+      {q: "What are 'pioneers' and 'ambassadors' ?",a:"'Prioneers' are the first 20 providers hand-picked by CanYa. 'Ambassadors' are first 200 providers after the pioneers. "},
+      {q: "What is CanYaCoin? (CAN)?",a:""},
+      {q: "Why do I have to use CAN?",a:""},
+      {q: "How do I buy CAN?",a:""},
+      {q: "Can I use BTC?",a:""},
+      {q: "What happens if there is a dispute?",a:"Initially CanYa will arbitrate to resolve the dispute. Disputes are rare, 1 in 1000 cases."},
+      {q: "How can I trust to get paid?",a:"All funds are held in our escrow smart contract so you will get paid upon completion."},
+      {q: "How can I cancel a contract?",a:""},
+      {q: "How do I control the notification settings I receive?",a:""},
+      {q: "What happens if crypto price changes?",a:""}
 
+  ]
   constructor() { }
 
   ngOnInit() {
--- src/app/core-components/footer/footer.component.ts
+++ src/app/core-components/footer/footer.component.ts
@@ -13,5 +13,5 @@ export class FooterComponent implements OnInit {
   ngOnInit() {
     createFooter();
   }
-
+ 
 }
