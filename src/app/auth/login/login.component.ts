94c7921c62229f19a5543a4c486cbc30104ab78e
Cam Stuart <cam.asoftware@gmail.com>
2018-07-12 15:32:21 +1000

    Remove uPort and clean up a little

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -11,7 +11,6 @@ import { environment } from '../../../environments/environment';
 import { User } from '../../core-classes/user';
 import { AuthService } from '../../core-services/auth.service';
 import { CanWorkEthService } from '../../core-services/eth.service';
-import { ScriptService } from '../../core-services/script.service';
 import { UserService } from '../../core-services/user.service';
 
 @Component({
@@ -36,15 +35,7 @@ export class LoginComponent implements OnInit, AfterViewInit {
     private userService: UserService,
     private afs: AngularFirestore,
     private http: Http,
-    private ethService: CanWorkEthService,
-    private script: ScriptService) {
-    this.script.load('uport').then(data => {
-      this.pageInit = true;
-      this.authService.initUport();
-    }).catch(error => {
-      this.pageInit = true;
-    });
-  }
+    private ethService: CanWorkEthService) { }
 
   ngOnInit() {
     const ua = window.navigator.userAgent;
@@ -56,25 +47,6 @@ export class LoginComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-  }
-
-  onUportLogin() {
-    try {
-      this.loading = true;
-      this.authService.uportConnectAsync().then((credentials) => {
-        const avatar = credentials['avatar'];
-        const randomAvatarUri = `assets/img/animals/${Math.floor(Math.random() * 109) + 1}.png`;
-        credentials['avatar'] = { 'uri': avatar ? credentials['avatar']['uri'] || randomAvatarUri : randomAvatarUri };
-        const parsedUser = new User(credentials);
-
-        this.handleLogin(parsedUser);
-      }, (err) => {
-        this.loading = false;
-      });
-    } catch (error) {
-      this.loading = false;
-      console.log('onConnect - error', error);
-    }
   }
 
   private setWeb3EthereumPublicAddress() {
@@ -178,24 +150,10 @@ export class LoginComponent implements OnInit, AfterViewInit {
   }
 
   handleLogin(userDetails: User) {
-    // let x: QueryFn = null;
-    // if (userDetails.email && userDetails.email !== 'Empty' && userDetails.email !== '') {
-    //   x = ref => ref.where('email', '==', userDetails.email).limit(1);
-    // }
     this.afs.collection<any>('users', ref => ref.where('address', '==', userDetails.address).limit(1)).valueChanges().take(1).subscribe((usersMatchingId: any) => {
       if (usersMatchingId && usersMatchingId.length > 0) {
         this.authService.setUser(usersMatchingId[0]);
         this.router.navigate([this.returnUrl]);
-
-        // } else if (x != null) {
-        //   this.afs.collection<any>('users', x).valueChanges().take(1).subscribe((usersMatchingEmail: any) => {
-        //     if (usersMatchingEmail && usersMatchingEmail.length > 0) {
-        //       this.authService.setUser(usersMatchingEmail[0]);
-        //       this.router.navigate(['/home']);
-        //     } else {
-        //       this.initialiseUserAndRedirect(userDetails);
-        //     }
-        //   });
       } else {
         this.initialiseUserAndRedirect(userDetails);
       }
