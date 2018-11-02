c3c8052823a059d17f3d09177170987fae883532
wie <wie.cbd@gmail.com>
2018-11-02 15:25:35 +1100

    basic CRUD for certification finished

--- src/app/core-services/certifications.service.ts
+++ src/app/core-services/certifications.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Certification } from '../core-classes/certification';
+import { Subscription } from 'rxjs/Subscription';
+
 @Injectable()
 export class CertificationsService {
   usersCollectionRef: AngularFirestoreCollection<any>;
   viewedUsersRef: AngularFirestoreCollection<any>;
+  certificationSub: Subscription;
   constructor(
     private afs: AngularFirestore
   ) {
   }
+
+  editCert = false;
+  certToEdit: Certification;
+
   /**
    * This service is created to accomodate the add certification feature on
    * the profile page, and to help easing the dock.io integration.
    */
+
   public addCertification(certification: Certification, userID: string) {
     console.log('adding Certification...');
     try {
@@ -30,11 +38,53 @@ export class CertificationsService {
     }
   }
 
-  public deleteCertification(certification: Certification, userID: string) {
+
+  public updateCertification(certification: Certification, userID: string) {
+    console.log('updating Certification...');
     try {
-      this.afs.doc(`users/${userID}/certifications/`).set(certification);
+      const tempCert = {
+        id: certification.id,
+        university: certification.university,
+        course: certification.course,
+        completion: certification.completion,
+        isStudying: certification.isStudying,
+        certificate: certification.certificate
+      }
+      this.afs.doc(`users/${userID}/certifications/${tempCert.id}`).update(tempCert);
     } catch (error) {
       console.error('submitForm - error', error);
     }
   }
+
+  public deleteCertification(certification: Certification, userID: string) {
+    try {
+      this.afs.doc(`users/${userID}/certifications/${certification.id}`).delete();
+    } catch (error) {
+      console.error('submitForm - error', error);
+    }
+  }
+
+  public async getCertifications(userID: string) {
+    console.log('fetching certifications...');
+    const certifications = this.afs.collection(`users/${userID}/certifications`);
+    let result: any;
+    this.certificationSub = certifications.valueChanges().subscribe((data: any) => {
+      result = data;
+      console.log(result);
+      return result;
+    });
+  }
+
+  public loadAddCert() {
+    console.log('Received request to add cert');
+    this.certToEdit = null;
+    this.editCert = false;
+  }
+  public loadEditCert(cert) {
+    console.log('Received edit for cert : ');
+    console.log(cert);
+    this.certToEdit = cert;
+    this.editCert = true;
+  }
+
 }
--- src/app/profile/components/certifications/certifications.component.ts
+++ src/app/profile/components/certifications/certifications.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { User } from '../../../core-classes/user';
+import { Subscription } from 'rxjs/Subscription';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Certification } from '../../../core-classes/certification';
+import { CertificationsService } from '../../../core-services/certifications.service';
+
 @Component({
   selector: 'app-certifications',
   templateUrl: './certifications.component.html',
@@ -11,14 +15,24 @@ export class CertificationsComponent implements OnInit {
   @Input() userModel: User;
   @Input() isMyProfile: boolean;
   @Input() notMyProfile: boolean;
+  @Output() editCertification = new EventEmitter();
 
+  userCertifications: any;
+  loaded = false;
+  certificationSub: Subscription;
   constructor(
+    private afs: AngularFirestore,
+    private certifications: CertificationsService
   ) {
   }
 
   ngOnInit() {
+    this.loadCertifications();
   }
 
+  OnDestroy() {
+    this.certificationSub.unsubscribe();
+  }
   onInputChange() {
     console.log('changed');
   }
@@ -27,8 +41,21 @@ export class CertificationsComponent implements OnInit {
     console.log('deleting');
   }
 
-  getCertifications() {
-    console.log('fetching certifications...');
+  async loadCertifications() {
+    const certifications = this.afs.collection(`users/${this.userModel.address}/certifications`);
+    this.certificationSub = certifications.valueChanges().subscribe((data: any) => {
+      this.userCertifications = data;
+      if (data.length >= 0) {
+        this.loaded = true;
+      }
+    });
+  }
+  setEditModal(cert) {
+    this.certifications.loadEditCert(cert);
+  }
+
+  setAddModal() {
+    this.certifications.loadAddCert();
   }
 
 }
