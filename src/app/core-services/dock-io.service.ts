165e1523da336ead40edfe696a1e808405f6c6d2
alsco77 <alsco77@pm.me>
2018-11-15 12:01:30 +0800

    Updated canpay lib to reslove canex issue and sanitised dock

--- src/app/core-services/dock-io.service.ts
+++ src/app/core-services/dock-io.service.ts
@@ -1,5 +1,5 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpParams, HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '@env/environment';
 
 @Injectable()
@@ -9,33 +9,33 @@ export class DockIoService {
     private http: HttpClient) { }
 
   get oAuthURI() {
-    const base = environment.dockio.oAuthURI
-    let params = new HttpParams()
-    params = params.set('client_id', environment.dockio.account.clientID)
-    params = params.set('redirect_uri', environment.dockio.oAuthRedirect)
-    params = params.set('response_type', 'code')
-    params = params.set('scope', environment.dockio.scope)
-    return `${base}?${params.toString()}`
+    const base = environment.dockio.oAuthURI;
+    let params = new HttpParams();
+    params = params.set('client_id', environment.dockio.account.clientID);
+    params = params.set('redirect_uri', environment.dockio.oAuthRedirect);
+    params = params.set('response_type', 'code');
+    params = params.set('scope', environment.dockio.scope);
+    return `${base}?${params.toString()}`;
   }
 
   getAccessTokenURI(code: string) {
-    const base = `${environment.dockio.client.accessTokenURI}/request-user-data`
-    let params = new HttpParams()
-    params = params.set('grant_type', 'authorization_code')
-    params = params.set('code', code)
-    params = params.set('client_id', environment.dockio.account.clientID)
-    params = params.set('client_secret', environment.dockio.account.clientSecret)
-    return `${base}?${params.toString()}`
+    const base = `${environment.dockio.client.accessTokenURI}/request-user-data`;
+    let params = new HttpParams();
+    params = params.set('grant_type', 'authorization_code');
+    params = params.set('code', code);
+    params = params.set('client_id', environment.dockio.account.clientID);
+    params = params.set('client_secret', environment.dockio.account.clientSecret);
+    return `${base}?${params.toString()}`;
   }
 
   callAuthenticationService(code: string) {
     const _headers = {
       'Authorization': `Bearer: ${window.sessionStorage.accessToken}`
-    }
+    };
     this.http.get(this.getAccessTokenURI(code), {
       headers: new HttpHeaders(_headers)
     }).subscribe((data) => {
-      console.log(data)
+      console.log(data);
     });
   }
 }
