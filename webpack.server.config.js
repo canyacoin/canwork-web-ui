0adddc06fd451e69ede3ec96ba5f883f1cf0abbd
Xinjie-canya <xinjie@canya.com>
2018-11-23 18:29:50 +0700

    fix errors

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,42 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
 
-    // Work around for https://github.com/angular/angular-cli/issues/7200
-    
-    const path = require('path');
-    const webpack = require('webpack');
-    
-    module.exports = {
-      entry: {
-        server: './server.ts',
+module.exports = {
+  entry: {
+    server: './server.ts',
+  },
+  target: 'node',
+  resolve: { extensions: ['.ts', '.js', '.node', '.json'] },
+  externals: [/(node_modules|main\..*\.js)/,],
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/, loader: 'ts-loader',
+        options: {
+          configFile: "./tsconfig.webpack.json"
+        }
       },
-      target: 'node',
-      resolve: { extensions: ['.ts', '.js'] },
-      externals: [/(node_modules|main\..*\.js)/,],
-      output: {
-        libraryTarget: 'commonjs2',
-        path: path.join(__dirname, 'dist'),
-        filename: '[name].js'
-      },
-      module: {
-        rules: [
-          { test: /\.ts$/, loader: 'ts-loader' }
-        ]
-      },
-      optimization: {
-        minimize: false
-      },
-      plugins: [
-        new webpack.ContextReplacementPlugin(
-          // fixes WARNING Critical dependency: the request of a dependency is an expression
-          /(.+)?angular(\\|\/)core(.+)?/,
-          path.join(__dirname, 'src'), // location of your src
-          {} // a map of your routes
-        ),
-        new webpack.ContextReplacementPlugin(
-          // fixes WARNING Critical dependency: the request of a dependency is an expression
-          /(.+)?express(\\|\/)(.+)?/,
-          path.join(__dirname, 'src'),
-          {}
-        )
-      ]
-    }
-    
+      { test: /\.node$/, loader: 'node-loader' }
+    ]
+  },
+  optimization: {
+    minimize: false
+  },
+  plugins: [
+    new webpack.ContextReplacementPlugin(
+      // fixes WARNING Critical dependency: the request of a dependency is an expression
+      /(.+)?angular(\\|\/)core(.+)?/,
+      path.join(__dirname, 'src'), // location of your src
+      {} // a map of your routes
+    ),
+    new webpack.ContextReplacementPlugin(
+      // fixes WARNING Critical dependency: the request of a dependency is an expression
+      /(.+)?express(\\|\/)(.+)?/,
+      path.join(__dirname, 'src'),
+      {}
+    )
+  ]
+}
