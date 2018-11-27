b55dcc5a163eb875e4c304d00eff77f538666fc8
wie <wie.cbd@gmail.com>
2018-11-27 20:14:00 +1100

    invite provider to job WIP

--- src/app/profile/components/about/about.component.ts
+++ src/app/profile/components/about/about.component.ts
@@ -2,8 +2,10 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 import { take } from 'rxjs/operators';
 import { User } from '@class/user';
+import { Job } from '@class/job';
 import { AuthService } from '@service/auth.service';
 import { ChatService } from '@service/chat.service';
+import { PublicJobService } from '@service/public-job.service';
 
 @Component({
   selector: 'app-profile-about',
@@ -16,17 +18,30 @@ export class AboutComponent implements OnInit {
   @Input() userModel: User;
   @Input() isMyProfile: boolean;
 
-  @Output() editProfile = new EventEmitter()
+  @Output() editProfile = new EventEmitter();
+
+  currentUserJobs = null;
+  pageLimit = 3;
+  currentPage = 0;
+  lastPage = 0;
+  animation = 'fadeIn';
 
   constructor(
     private router: Router,
     private authService: AuthService,
-    private chatService: ChatService) { }
+    private chatService: ChatService,
+    private publicJobService: PublicJobService) { }
 
-  ngOnInit() { }
+  async ngOnInit() {
+    this.publicJobService.getPublicJobsByUser(this.currentUser.address).subscribe(async (jobs: Job[]) => {
+      this.currentUserJobs = jobs.filter(job => job.state === 'Accepting Offers' && job.draft !== true);
+      this.lastPage = (Math.ceil(this.currentUserJobs.length / this.pageLimit) - 1);
+      console.log(this.currentUserJobs);
+    });
+  }
 
   displayProfileEditComponent() {
-    this.editProfile.emit(true)
+    this.editProfile.emit(true);
   }
 
   proposeJob() {
@@ -37,6 +52,27 @@ export class AboutComponent implements OnInit {
         this.router.navigate(['auth/login']);
       }
     });
+  }
+
+  paginatedUserJobs() {
+    const paginated = this.currentUserJobs.slice((this.currentPage * this.pageLimit), ((this.currentPage * this.pageLimit) + this.pageLimit));
+    return paginated;
+  }
+
+  nextPage() {
+    this.animation = 'fadeOut';
+    setTimeout(() => {
+      this.currentPage++;
+      this.animation = 'fadeIn';
+    }, 300);
+  }
+
+  previousPage() {
+    this.animation = 'fadeOut';
+    setTimeout(() => {
+      this.currentPage--;
+      this.animation = 'fadeIn';
+    }, 300);
   }
 
   // Chat the user without proposing a job
