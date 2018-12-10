553afb95751b71c6c98b09e731fdb1d2e9584cd6
Xinjie-canya <xinjie@canya.com>
2018-12-10 21:45:53 +0800

    notify loser

--- src/app/public-job/job-bids/job-bids.component.ts
+++ src/app/public-job/job-bids/job-bids.component.ts
@@ -102,6 +102,9 @@ export class JobBidsComponent implements OnInit {
       const chosen = await this.publicJobsService.declineBid(this.job, bid);
       if (chosen) {
         alert('Declined!');
+        const client = await this.userService.getUser(this.job.clientId);
+        this.publicJobsService.notifyLosers(this.job, client, [bid]);
+        this.router.navigate(['/inbox/job', this.job.id]);
       } else {
         alert('Something went wrong. please try again later');
       }
