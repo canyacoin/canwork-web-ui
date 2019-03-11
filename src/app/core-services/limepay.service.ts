4a6d12617fc756057f418a3ea54d835c0df5856d
wie <wie.cbd@gmail.com>
2019-03-11 14:06:36 +1100

    added placeholder createWallet() function

--- src/app/core-services/limepay.service.ts
+++ src/app/core-services/limepay.service.ts
@@ -1,7 +1,7 @@
+import { AuthService } from '@service/auth.service';
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers } from '@angular/http';
 import { environment } from '@env/environment';
-
 
 const apiUrl = environment.limepay.uri;
 
@@ -17,11 +17,30 @@ const httpOptions = {
 })
 export class LimepayService {
 
-  constructor(private http: Http) { }
+  constructor(
+    private http: Http,
+    private auth: AuthService
+  ) { }
 
   async getWallet() {
     try {
       const res = await this.http.get(`${apiUrl}/getWallet`, httpOptions).take(1).toPromise();
+      return Promise.resolve(res.json());
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  }
+
+  async createWallet(password) {
+    try {
+      const token = await this.auth.getJwt();
+      const options = {
+        headers: new Headers({
+          'Content-Type': 'application/json',
+          'Authorization': token
+        })
+      };
+      const res = await this.http.post(`${apiUrl}/createWallet`, password, options).take(1).toPromise();
       return Promise.resolve(res.json());
     } catch (e) {
       return Promise.reject(e);
