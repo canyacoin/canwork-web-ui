cc039329ae397842231ec5ba02a1c8213d6732d4
Chris McLoughlin <chris@canya.com>
2019-05-28 07:36:51 +0000

    added urls

--- src/app/core-components/faq-page/faq-page.component.ts
+++ src/app/core-components/faq-page/faq-page.component.ts
@@ -9,23 +9,23 @@ export class FaqPageComponent implements OnInit {
   faqs = [
     {
       index: 'q-1',
-      q: 'Who is CanWork for?',
-      a: 'CanWork is a platform for clients to find quality providers who offer digital services (eg. software engineering, graphics design, content writing, digital marketing). '
+      q: 'Who is CANwork for?',
+      a: 'CANWork is a platform for clients to find quality providers who offer digital services (eg. software engineering, graphics design, content writing, digital marketing). '
     },
     {
       index: 'q-2',
-      q: 'Why CanWork?',
-      a: 'CanWork only takes 1% in service fees when you make a transaction. This means that when you receive payment for your work, you take more of your hard earned salary home. You can expect high quality providers from CANwork. This because all of our providers are hand picked and vetted for their skills. CANwork is decentralised  - CANWork does not and will never keep or distribute your personal data. CANwork provides fantastic utility for the many unbanked agents in the developing world. Cryptocurrency payments allow for international transactions to be processed quickly and with less fees. Payments on CANwork are enabled for use globally. Those in countries where paypal is not available don’t have to worry when using our platform because our transactions are all made using cryptocurrency. National currency such as USD and AUD transactions coming soon. CANwork has a global reach because of the borderless nature of crypto payments.  This is particularly beneficial as it gives everyone equal opportunity to access the global gig economy, and most importantly equal opportunity to earn an equivalent wage. CANwork is continually being optimised for your experience. CANwork is made from insights from our community and is a platform for gig workers, designed by gig workers. CANwork has created a suite of free tools which allows a sole operator to build and operate their business from their own computer.'
+      q: 'Why CANwork?',
+      a: 'CANwork only takes a 1% payment fee when you make a transaction. This means that when you receive payment for your work, you take more of your hard earned salary home. You can expect high quality providers from CANwork. This because all of our providers are hand picked and vetted for their skills. CANwork is decentralised  - CANWork does not and will never keep or distribute your personal data. CANwork provides fantastic utility for the many unbanked agents in the developing world. Cryptocurrency payments allow for international transactions to be processed quickly and with less fees. Payments on CANwork are enabled for use globally. Those in countries where paypal is not available don’t have to worry when using our platform because our transactions are all made using cryptocurrency. National currency such as USD and AUD transactions coming soon. CANwork has a global reach because of the borderless nature of crypto payments.  This is particularly beneficial as it gives everyone equal opportunity to access the global gig economy, and most importantly equal opportunity to earn an equivalent wage. CANwork is continually being optimised for your experience. CANwork is made from insights from our community and is a platform for gig workers, designed by gig workers. CANwork has created a suite of free tools which allows a sole operator to build and operate their business from their own computer.'
     },
     {
       index: 'q-3',
       q: 'How do I get started as a provider?',
-      a: 'Join CanWork and set up your account as a provider. You will then receive an email to get started with completing your profile. When your signing up as a provider you will need to be whitelisted, which can take up to 72 hours to determine if your eligible to be a provider.'
+      a: 'Join CANwork and set up your account as a provider. You will then receive an email to get started with completing your profile. When your signing up as a provider you will need to be whitelisted, which can take up to 72 hours to determine if your eligible to be a provider. For more information on setting up a provider profile read this: https://medium.com/canyacoin/get-started-as-a-canwork-freelancer-2fcaf41fedf2?source=your_stories_page'
     },
     {
       index: 'q-4',
       q: 'How do I get started as a client?',
-      a: 'Simply join CanWork and set your account up as a client. You will then have full access to the platform, and be able to communicate with any service provider. '
+      a: 'Simply join CANwork and set your account up as a client. You will then have full access to the platform, and be able to communicate with any service provider. For more information on using the platform as a client for the first time read this: https://medium.com/canyacoin/get-started-as-a-canwork-client-ffb3acab1cde '
     },
     {
       index: 'q-5',
@@ -40,7 +40,7 @@ export class FaqPageComponent implements OnInit {
     {
       index: 'q-7',
       q: 'Why do I have to use CAN?',
-      a: 'CAN token is the native token of the CanYa Ecosystem, providing a bridge between other currencies. The CAN token is integrated into the hedged escrow, ensuring that all transactions on the platform are hedged and free from price volatility.'
+      a: 'CAN token is the native token of the CanYa Ecosystem, providing a bridge between other currencies. The CAN token is integrated into the hedged escrow, ensuring that all transactions on the platform are hedged and free from price volatility. For a global freelance marketplace $CAN is also useful as its value remains universal across the globe and can be sent to anyone, anywhere at any time.'
     },
     {
       index: 'q-8',
@@ -65,7 +65,7 @@ export class FaqPageComponent implements OnInit {
     {
       index: 'q-12',
       q: 'What happens if crypto price changes?',
-      a: 'CanYa’s hedged escrow service mitigates against market volatility. If you’ve quoted, for example, $500 USD in CAN tokens for a job, you will receive the same value of $500 USD in CAN upon completing the job regardless of changes in CanYaCoin price.'
+      a: 'CanYa’s hedged escrow service mitigates against market volatility. If you’ve quoted, for example, $500 USD in CAN tokens for a job, you will receive the same value of $500 USD in CAN upon completing the job regardless of changes in CanYaCoin price. You can read more about the Hedged Escrow here: https://medium.com/@canyacoin/canya-releases-hedged-escrow-to-protect-platform-users-from-price-volatility-7fca7a0aad32?source=linkShare-8ab1feddf31a-1536654577'
     },
     {
       index: 'q-13',
@@ -85,7 +85,7 @@ export class FaqPageComponent implements OnInit {
     {
       index: 'q-16',
       q: 'What wallet should I use?',
-      a: 'Use metamask for payments.'
+      a: 'Use metamask for payments. Here is a guide to set up a MetaMask wallet: https://medium.com/canyacoin/setting-up-a-metamask-wallet-to-use-with-canwork-151ed877ac54'
     },
     {
       index: 'q-18',
@@ -94,8 +94,8 @@ export class FaqPageComponent implements OnInit {
     },
     {
       index: 'q-19',
-      q: 'Can anybody be apply to be a provider ?',
-      a: 'Yes! However providers will be screened through the CanYa support for QA.'
+      q: 'Can anybody apply to be a provider ?',
+      a: 'Yes! However providers will be screened through the CanYa support for quality assurance.'
     },
     {
       index: 'q-20',
