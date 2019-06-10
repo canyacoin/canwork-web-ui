0c1d995673a21073c714954d5871ca5e1866caeb
Jungle <devex.soft@gmail.com>
2019-06-10 12:51:22 +0500

    feat(jobs): Add prepareJobRefs func

--- functions/src/remove-old-data.ts
+++ functions/src/remove-old-data.ts
@@ -1,16 +1,30 @@
-import { Firestore, DocumentReference } from '@google-cloud/firestore'
+import {
+  Firestore,
+  DocumentReference,
+  QuerySnapshot,
+} from '@google-cloud/firestore'
+
+function getRefsFromSnapshot(snap: QuerySnapshot): DocumentReference[] {
+  return snap.docs.reduce((acc, { ref }) => {
+    acc.push(ref)
+    return acc
+  }, [])
+}
 
 const delta = 365 * 24 * 60 * 60 * 1000
-export async function prepareData(
+export async function prepareJobRefs(
   db: Firestore,
+  collection: string,
   limit = 500,
   timestamp = Date.now() - delta
 ) {
   const jobsSnap = await db
-    .collection('jobs')
+    .collection(collection)
     .where('createAt', '<', timestamp)
     .limit(limit)
     .get()
+
+  return getRefsFromSnapshot(jobsSnap)
 }
 
 export function removeOldData(db: Firestore, refs: DocumentReference[]) {
