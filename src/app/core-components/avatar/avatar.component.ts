f70f41deb9ab4d652bfad660df53b05fed034d44
Vladislav Timofeev <vtimofeev.m@gmail.com>
2019-06-19 16:24:21 +0700

    pass custom class to avatar

--- src/app/core-components/avatar/avatar.component.ts
+++ src/app/core-components/avatar/avatar.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class AvatarComponent implements OnInit {
 
   @Input() user: any;
+  @Input() customClass: string;
 
   constructor() { }
 
