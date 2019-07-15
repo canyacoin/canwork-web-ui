8daba7f9f2ce089dad59ddc07d6e6bfabdb93d06
Jungle <devex.soft@gmail.com>
2019-07-15 10:42:51 +0500

    fix(eth validator): Fix EthereumValidator.isUniqueAddress
    #406

--- src/app/@validator/ethereum.validator.ts
+++ src/app/@validator/ethereum.validator.ts
@@ -1,51 +1,29 @@
-import { FormControl } from '@angular/forms';
-import { User } from '@class/user';
-import { AngularFirestoreCollection } from 'angularfire2/firestore';
-import { EthService } from '@service/eth.service';
-
-declare let require: any;
-const Web3 = require('web3');
+import { FormControl } from '@angular/forms'
+import { User } from '@class/user'
+import { AngularFirestoreCollection } from 'angularfire2/firestore'
+import { EthService } from '@service/eth.service'
 
 export class EthereumValidator {
-  static ethService: EthService;
-  constructor(private ethService: EthService) {
-    EthereumValidator.ethService = ethService;
-  }
-  isValidAddress(control: FormControl) {
+  constructor(private ethService: EthService) {}
 
-    if (EthereumValidator.ethService.isAddress(control.value)) {
-      return null;
-    }
-
-    return {
-      isInvalidEthereumAddress: true
-    };
+  isValidAddress = (control: FormControl) => {
+    return this.ethService.isAddress(control.value)
+      ? null
+      : { isInvalidEthereumAddress: true }
   }
 
-  isUniqueAddress(usersCollection: AngularFirestoreCollection<any>, user: User) {
+  isUniqueAddress(
+    usersCollection: AngularFirestoreCollection<any>,
+    user: User
+  ) {
     return async (control: FormControl) => {
       const data = await usersCollection.ref
-        .where('ethAddressLookup', '==', control.value.toUpperCase()).get();
+        .where('ethAddressLookup', '==', control.value.toUpperCase())
+        .get()
 
-      if (data.empty) {
-        return null;
-      }
-
-      return new Promise((resolve, reject) => {
-        data.forEach(record => {
-
-          const addressBelongsToUser: boolean = record.id === user.address &&
-            control.value.toUpperCase() === user.ethAddress.toUpperCase();
-          // console.log('is ' + control.value.toUpperCase() + '===' + user.ethAddress.toUpperCase() + ' ? ' + addressBelongsToUser);
-          if (addressBelongsToUser) {
-            resolve(null);
-          }
-
-          resolve({
-            addressExists: true
-          });
-        });
-      });
-    };
+      return data.docs.some(item => item.get('address') !== user.address)
+        ? { addressExists: true }
+        : null
+    }
   }
 }
