cd8c456dba11262e4180429abf276d4dfd38269b
Jungle <devex.soft@gmail.com>
2019-07-26 12:37:09 +0500

    fix(avatar): Fix avatar
    #420

--- src/app/core-components/avatar/avatar.component.ts
+++ src/app/core-components/avatar/avatar.component.ts
@@ -1,35 +1,46 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core'
+import { Observable, from } from 'rxjs'
 
 @Component({
   selector: 'app-avatar',
   templateUrl: './avatar.component.html',
-  styleUrls: ['./avatar.component.css']
+  styleUrls: ['./avatar.component.css'],
 })
 export class AvatarComponent implements OnInit {
+  @Input() user: any
+  @Input() customClass: string
 
-  @Input() user: any;
-  @Input() customClass: string;
+  avatarUrl: Observable<string>
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() { }
-
-  getBackground() {
-    if (this.user && this.user.avatar && this.user.avatar.uri) {
-      return `url(${this.user.avatar.uri})`;
-    }
-    return null;
+  ngOnInit() {
+    const url = this.user && this.user.avatar && this.user.avatar.uri
+    this.avatarUrl = from(
+      new Promise<string>((resolve, reject) => {
+        if (!url) {
+          reject(new Error('Missing avatar URL'))
+          return
+        }
+        const img = new Image()
+        img.onload = () => {
+          resolve(url)
+        }
+        img.onerror = reject
+        img.src = url
+      })
+    )
   }
 
   getInitials() {
     if (this.user && this.user.name) {
       return this.user.name
-      .split(' ')
-      .map(str => str[0])
-      .join('')
-      .substring(0, 2)
-      .toUpperCase();
+        .split(' ')
+        .map(str => str[0])
+        .join('')
+        .substring(0, 2)
+        .toUpperCase()
     }
-    return '';
+    return ''
   }
 }
