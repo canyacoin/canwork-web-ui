201f6b9f9ca2a5859f55d78ea54d22e356fcf665
Jungle <devex.soft@gmail.com>
2019-08-24 17:53:52 +0500

    test(fsr tests): Add tests for `portfolio` collection

--- tests/security-rules.test.ts
+++ tests/security-rules.test.ts
@@ -133,92 +133,49 @@ describe('Test `portfolio` collection rules', () => {
   afterAll(async () => {
     await teardown()
   })
-  // deny tests for Anonym
-  test('deny Anonym to create a portfolio', async () => {
-    const db = await setup(auth.anonym, mockData, rules)
+  const path = 'portfolio/alice/work/1'
+  const data = {
+    'portfolio/alice/work/1': {
+      description: 'text description',
+      title: 'Alice title',
+    },
+  }
 
-    await expect(
-      db.doc('portfolio/alice/work/1').set({ description: 'description' })
-    ).toDeny()
-  })
+  allow(rules, path, auth.alice, data)
+    .read()
+    .create({
+      description: 'text description',
+      title: 'Alice title',
+    })
+    .update({
+      description: 'text description',
+      title: 'Alice title',
+    })
+    .delete()
 
-  test('deny Anonym to update a portfolio', async () => {
-    const db = await setup(auth.anonym, mockData, rules)
+  deny(rules, path, auth.bob, data, 'none own portfolio')
+    .create({
+      description: 'text description',
+      title: 'Alice title',
+    })
+    .update({
+      description: 'text description',
+      title: 'Alice title',
+    })
+    .delete()
+    .allow()
+    .read()
 
-    await expect(
-      db.doc('portfolio/alice/work/1').update({ description: 'description' })
-    ).toDeny()
-  })
-
-  test('deny Anonym to delete a portfolio', async () => {
-    const db = await setup(auth.anonym, mockData, rules)
-
-    await expect(db.doc('portfolio/alice/work/1').delete()).toDeny()
-  })
-
-  // deny tests for Bob
-  test('deny Bob to create a portfolio with fake `userId`', async () => {
-    const db = await setup(auth.bob, mockData, rules)
-
-    await expect(
-      db.doc('portfolio/alice/work/1').set({ description: 'description' })
-    ).toDeny()
-  })
-
-  test('deny Bob to update Alice portfolio', async () => {
-    const db = await setup(auth.bob, mockData, rules)
-
-    await expect(
-      db.doc('portfolio/alice/work/1').update({ description: 'description' })
-    ).toDeny()
-  })
-
-  test('deny Bob to delete Alice portfolio', async () => {
-    const db = await setup(auth.bob, mockData, rules)
-
-    await expect(db.doc('portfolio/alice/work/1').delete()).toDeny()
-  })
-
-  // allow tests
-  test('allow Alice to create a portfolio', async () => {
-    const db = await setup(auth.alice, mockData, rules)
-
-    await expect(
-      db.doc('portfolio/alice/work/100').set({ description: 'description' })
-    ).toAllow()
-  })
-
-  test('allow Alice to update a portfolio', async () => {
-    const db = await setup(auth.alice, mockData, rules)
-
-    await expect(
-      db
-        .doc('portfolio/alice/work/1')
-        .update({ description: 'change description' })
-    ).toAllow()
-  })
-
-  test('allow Alice to delete a portfolio', async () => {
-    const db = await setup(auth.alice, mockData, rules)
-
-    await expect(db.doc('portfolio/alice/work/1').delete()).toAllow()
-  })
-
-  test('allow Alice to read self portfolio', async () => {
-    const db = await setup(auth.alice, mockData, rules)
-
-    await expect(db.doc('portfolio/alice/work/1').get()).toAllow()
-  })
-
-  test('allow Alice to read Bob portfolio', async () => {
-    const db = await setup(auth.alice, mockData, rules)
-
-    await expect(db.doc('portfolio/bob/work/1').get()).toAllow()
-  })
-
-  test('allow Anonym to read a portfolio', async () => {
-    const db = await setup(auth.anonym, mockData, rules)
-
-    await expect(db.doc('portfolio/bob/work/1').get()).toAllow()
-  })
+  deny(rules, path, auth.anonym, data)
+    .create({
+      description: 'text description',
+      title: 'Alice title',
+    })
+    .update({
+      description: 'text description',
+      title: 'Alice title',
+    })
+    .delete()
+    .allow()
+    .read()
 })
