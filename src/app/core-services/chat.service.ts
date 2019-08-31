22d4ea0465cc9c7741e204970f69fe0f10364ac1
Vladislav Timofeev <vtimofeev.m@gmail.com>
2019-08-31 17:07:53 +0700

    fix unread message detection on new channel creation

--- src/app/core-services/chat.service.ts
+++ src/app/core-services/chat.service.ts
@@ -63,7 +63,7 @@ export class ChatService {
     const channelId: string = [sender.address, receiver.address]
       .sort()
       .join('-')
-    const senderChannel = this.createChannelObject(channelId, receiver, false)
+    const senderChannel = this.createChannelObject(channelId, receiver)
     const receiverChannel = this.createChannelObject(channelId, sender)
     const senderChannelCreated = await this.saveChannelAsync(
       sender.address,
@@ -295,11 +295,7 @@ export class ChatService {
     }
   }
 
-  private createChannelObject(
-    channelId: string,
-    user: User,
-    unreadMessages: boolean = true
-  ): Channel {
+  private createChannelObject(channelId: string, user: User): Channel {
     return new Channel({
       channel: channelId,
       address: user.address,
@@ -307,7 +303,7 @@ export class ChatService {
       name: user.name,
       title: user.title,
       message: '',
-      unreadMessages: unreadMessages,
+      unreadMessages: false,
       timestamp: Date.now(),
     })
   }
