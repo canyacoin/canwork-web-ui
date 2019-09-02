76712a6ee6238a900cb16529cbf40515f83b4590
Jungle <devex.soft@gmail.com>
2019-09-02 16:32:23 +0500

    test(fsr): Add tests for `invites` subcollection

--- tests/public-jobs.test.ts
+++ tests/public-jobs.test.ts
@@ -43,13 +43,74 @@ describe('Test `public-jobs` collection rules (visibility invite)', () => {
 
   allow({ name: 'Alice tests', rules, path, auth: auth.alice, data })
     .read()
+    .create({
+      data: { clientId: 'alice', providerId: 'bob', budget: 50 },
+      id: '2',
+    })
+    .update({ data: { budget: 150 } })
+    .deny()
+    .delete()
     .runTests()
 
   allow({ name: 'Bob tests (invited user)', rules, path, auth: auth.bob, data })
     .read()
+    .deny()
+    .update()
+    .delete()
+    .runTests()
+
+  deny({
+    name: 'John tests (not invited user)',
+    rules,
+    path,
+    auth: auth.john,
+    data,
+  })
+    .read()
+    .update()
+    .delete()
     .runTests()
 
   deny({ name: 'Anonym tests', rules, path, auth: auth.anonym, data })
     .read()
+    .update()
+    .delete()
+    .runTests()
+})
+
+describe('Test `invites` subcollection rules', () => {
+  const path = 'public-jobs/1/invites/bob'
+  const data = {
+    'public-jobs/1': {
+      clientId: 'alice',
+      budget: 100,
+      visibility: 'invite',
+    },
+    [path]: {
+      providerId: 'bob',
+    },
+  }
+
+  allow({ name: 'Alice tests', rules, path, auth: auth.alice, data })
+    .read()
+    .create({ data: { providerId: 'bob' } })
+    .update({ data: { providerId: 'bob' } })
+    .deny()
+    .delete()
+    .runTests()
+
+  allow({ name: 'Bob tests (invited user)', rules, path, auth: auth.bob, data })
+    .read()
+    .deny()
+    .create({ id: 'alice' })
+    .update()
+    .delete()
+    .runTests()
+
+  deny({ name: 'Anonym tests', rules, path, auth: auth.anonym, data })
+    .read()
+    .create()
+    .update()
+    .delete()
     .runTests()
 })
