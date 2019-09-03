fe1933170a3cb64cddafef7ddffccdd791171ced
Jungle <devex.soft@gmail.com>
2019-09-03 10:52:32 +0500

    fix(app.module.ts): Fix missing AngularFireFunctionModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,45 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CanpayModule } from '@canpay-lib/canpay.module';
-import { StarRatingModule } from 'angular-star-rating';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireStorageModule } from 'angularfire2/storage';
-import { Ng5SliderModule } from 'ng5-slider';
-import { NgxPaginationModule } from 'ngx-pagination';
-import { FirebaseUIModule } from 'firebaseui-angular';
-import { environment } from '../environments/environment';
-import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app.routing.module';
-import { CoreComponentsModule } from './core-components/core-components.module';
-import { firebaseUiAuthConfig } from './core-config/app-auth-config';
-import { AuthService } from './core-services/auth.service';
-import { CertificationsService } from './core-services/certifications.service';
-import { CoreServicesModule } from './core-services/core-services.module';
-import { DockIoService } from './core-services/dock-io.service';
-import { EthService } from './core-services/eth.service';
-import { JobNotificationService } from './core-services/job-notification.service';
-import { MobileService } from './core-services/mobile.service';
-import { NavService } from './core-services/nav.service';
-import { PublicJobService } from './core-services/public-job.service';
-import { CoreUtilsModule } from './core-utils/core-utils.module';
-import { PublicJobComponent } from './public-job/public-job/public-job.component';
-import { DashboardComponent } from './public-job/dashboard/dashboard.component';
-import { JobBidsComponent } from './public-job/job-bids/job-bids.component';
-import { FilterPipeModule } from 'ngx-filter-pipe';
-import { OrderModule } from 'ngx-order-pipe';
+import { HttpClientModule } from '@angular/common/http'
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { CanpayModule } from '@canpay-lib/canpay.module'
+import { StarRatingModule } from 'angular-star-rating'
+import { AngularFireModule } from 'angularfire2'
+import { AngularFireAuthModule } from 'angularfire2/auth'
+import { AngularFirestoreModule } from 'angularfire2/firestore'
+import { AngularFireStorageModule } from 'angularfire2/storage'
+import { Ng5SliderModule } from 'ng5-slider'
+import { NgxPaginationModule } from 'ngx-pagination'
+import { FirebaseUIModule } from 'firebaseui-angular'
+import { environment } from '../environments/environment'
+import { AppComponent } from './app.component'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { AppRoutingModule } from './app.routing.module'
+import { CoreComponentsModule } from './core-components/core-components.module'
+import { firebaseUiAuthConfig } from './core-config/app-auth-config'
+import { AuthService } from './core-services/auth.service'
+import { CertificationsService } from './core-services/certifications.service'
+import { CoreServicesModule } from './core-services/core-services.module'
+import { DockIoService } from './core-services/dock-io.service'
+import { EthService } from './core-services/eth.service'
+import { JobNotificationService } from './core-services/job-notification.service'
+import { MobileService } from './core-services/mobile.service'
+import { NavService } from './core-services/nav.service'
+import { PublicJobService } from './core-services/public-job.service'
+import { CoreUtilsModule } from './core-utils/core-utils.module'
+import { PublicJobComponent } from './public-job/public-job/public-job.component'
+import { DashboardComponent } from './public-job/dashboard/dashboard.component'
+import { JobBidsComponent } from './public-job/job-bids/job-bids.component'
+import { FilterPipeModule } from 'ngx-filter-pipe'
+import { OrderModule } from 'ngx-order-pipe'
+import { AngularFireFunctionsModule } from '@angular/fire/functions'
 
 @NgModule({
   declarations: [
     AppComponent,
     PublicJobComponent,
     JobBidsComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
   imports: [
     AppRoutingModule,
@@ -46,6 +47,7 @@ import { OrderModule } from 'ngx-order-pipe';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireStorageModule,
+    AngularFireFunctionsModule,
     FirebaseUIModule.forRoot(firebaseUiAuthConfig),
     BrowserModule,
     BrowserAnimationsModule,
@@ -60,11 +62,9 @@ import { OrderModule } from 'ngx-order-pipe';
     ReactiveFormsModule,
     OrderModule,
     NgxPaginationModule,
-    StarRatingModule.forRoot()
+    StarRatingModule.forRoot(),
   ],
-  exports: [
-    FilterPipeModule
-  ],
+  exports: [FilterPipeModule],
   providers: [
     AuthService,
     MobileService,
@@ -73,8 +73,8 @@ import { OrderModule } from 'ngx-order-pipe';
     JobNotificationService,
     CertificationsService,
     DockIoService,
-    PublicJobService
+    PublicJobService,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
