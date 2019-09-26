1208de237f8efbeecb041598735245c1f92a890b
Jungle <devex.soft@gmail.com>
2019-09-26 09:32:13 +0500

    feat(WalletBnb): Add wallet tabs

--- src/app/wallet-bnb/wallet-bnb.component.ts
+++ src/app/wallet-bnb/wallet-bnb.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+
+enum Wallet {
+  Connect,
+  Ledger,
+  Keystore,
+  Mnemonic,
+}
 
 @Component({
   selector: 'app-wallet-bnb',
   templateUrl: './wallet-bnb.component.html',
-  styleUrls: ['./wallet-bnb.component.css']
+  styleUrls: ['./wallet-bnb.component.css'],
 })
 export class WalletBnbComponent implements OnInit {
+  selected: Wallet = Wallet.Connect
+  Wallet = Wallet
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  isActive(wallet: Wallet): boolean {
+    return this.selected == wallet
   }
-
 }
