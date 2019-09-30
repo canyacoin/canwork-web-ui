b30190612f84cecd95241dfe2f764b99d559d0c2
Jungle <devex.soft@gmail.com>
2019-09-30 15:20:20 +0500

    refactor(BinanceService): Move UI logic to WalletBnb component

--- src/app/core-services/binance.service.ts
+++ src/app/core-services/binance.service.ts
@@ -1,68 +1,42 @@
 import { Injectable } from '@angular/core'
-import { Account } from '@trustwallet/types'
 import WalletConnect from '@trustwallet/walletconnect'
-import WalletConnectQRCodeModal from '@walletconnect/qrcode-modal'
+
+export type Connector = WalletConnect
+export enum WalletApp {
+  WalletConnect,
+  Ledger,
+  Keystore,
+  Mnemonic,
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class BinanceService {
+  static connector: Connector | null
   static walletConnector: WalletConnect
   constructor() {}
 
-  connectWalletConnect(): Promise<Account> {
-    return new Promise(async (resolve, reject) => {
-      // Create a walletConnector
-      const walletConnector = new WalletConnect({
-        bridge: 'https://bridge.walletconnect.org', // Required
-      })
-      BinanceService.walletConnector = walletConnector
+  async connect(app: WalletApp): Promise<Connector> {
+    switch (app) {
+      case WalletApp.WalletConnect:
+        BinanceService.connector = await this.initWalletConnect()
+        break
+    }
 
-      // Subscribe to connection events
-      walletConnector.on('connect', async error => {
-        if (error) {
-          reject(error)
-        }
-        // Close QR Code Modal
-        WalletConnectQRCodeModal.close()
-        // Get provided accounts and chainId
-        const accounts = await walletConnector.getAccounts()
-        console.log(accounts)
-        resolve(accounts.find(account => account.network == 714))
-      })
+    return BinanceService.connector
+  }
 
-      walletConnector.on('disconnect', error => {
-        if (error) {
-          reject(error)
-        }
-        // Delete walletConnector
-        this.disconnect()
-      })
-
-      // Check if connection is already established
-      if (!walletConnector.connected) {
-        // create new session
-        await walletConnector.createSession()
-        // get uri for QR Code modal
-        const uri = walletConnector.uri
-        // display QR Code modal
-        WalletConnectQRCodeModal.open(uri, () => {
-          console.log('QR Code Modal closed')
-        })
-
-        // hack
-        setTimeout(() => {
-          const qrModal = document.getElementById('walletconnect-qrcode-modal')
-          if (qrModal) {
-            qrModal.style.zIndex = '99999'
-          }
-        }, 100)
-      }
+  async initWalletConnect(): Promise<WalletConnect> {
+    // Create a walletConnector
+    const connector = new WalletConnect({
+      bridge: 'https://bridge.walletconnect.org', // Required
     })
+    return connector
   }
 
   disconnect() {
     console.log('Disconnect')
-    BinanceService.walletConnector = undefined
+    BinanceService.connector = null
   }
 }
--- src/app/wallet-bnb/wallet-bnb.component.ts
+++ src/app/wallet-bnb/wallet-bnb.component.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core'
-import { BinanceService } from '@service/binance.service'
-
-enum Wallet {
-  Connect,
-  Ledger,
-  Keystore,
-  Mnemonic,
-}
+import { BinanceService, WalletApp } from '@service/binance.service'
+import WalletConnect from '@trustwallet/walletconnect'
+import { Account } from '@trustwallet/types'
+import WalletConnectQRCodeModal from '@walletconnect/qrcode-modal'
 
 @Component({
   selector: 'app-wallet-bnb',
@@ -14,8 +10,8 @@ enum Wallet {
   styleUrls: ['./wallet-bnb.component.css'],
 })
 export class WalletBnbComponent implements OnInit, OnDestroy {
-  selected: Wallet = Wallet.Connect
-  Wallet = Wallet
+  selected: WalletApp = WalletApp.WalletConnect
+  WalletApp = WalletApp
 
   constructor(private binanceService: BinanceService) {}
 
@@ -25,12 +21,60 @@ export class WalletBnbComponent implements OnInit, OnDestroy {
     this.binanceService.disconnect()
   }
 
-  isActive(wallet: Wallet): boolean {
+  isActive(wallet: WalletApp): boolean {
     return this.selected == wallet
   }
 
-  async connectWalletConnect() {
-    const account = await this.binanceService.connectWalletConnect()
-    console.log(account)
+  async connect(app: WalletApp) {
+    const connector = await this.binanceService.connect(app)
+    let account: Account
+    if (connector instanceof WalletConnect) {
+      account = await this.walletConnect(connector)
+    }
+  }
+
+  walletConnect(connector: WalletConnect): Promise<Account> {
+    return new Promise(async (resolve, reject) => {
+      // Subscribe to connection events
+      connector.on('connect', async error => {
+        if (error) {
+          reject(error)
+        }
+        // Close QR Code Modal
+        WalletConnectQRCodeModal.close()
+        // Get provided accounts and chainId
+        const accounts = await connector.getAccounts()
+        console.log(accounts)
+        resolve(accounts.find(account => account.network == 714))
+      })
+
+      connector.on('disconnect', error => {
+        if (error) {
+          reject(error)
+        }
+        // Delete walletConnector
+        // this.disconnect()
+      })
+
+      // Check if connection is already established
+      if (!connector.connected) {
+        // create new session
+        await connector.createSession()
+        // get uri for QR Code modal
+        const uri = connector.uri
+        // display QR Code modal
+        WalletConnectQRCodeModal.open(uri, () => {
+          console.log('QR Code Modal closed')
+        })
+
+        // hack
+        setTimeout(() => {
+          const qrModal = document.getElementById('walletconnect-qrcode-modal')
+          if (qrModal) {
+            qrModal.style.zIndex = '99999'
+          }
+        }, 100)
+      }
+    })
   }
 }
