8acd0cb687fa0601ec5c328c505075ae6366b766
Vladislav Timofeev <vtimofeev.m@gmail.com>
2019-11-25 20:43:54 +0700

    add keystore tx scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ import { FilterPipeModule } from 'ngx-filter-pipe'
 import { OrderModule } from 'ngx-order-pipe'
 import { AngularFireFunctionsModule } from '@angular/fire/functions'
 import { WalletBnbComponent } from './wallet-bnb/wallet-bnb.component'
+import { KeystoreTxComponent } from './keystore-tx/keystore-tx.component'
 import { BinanceService } from '@service/binance.service'
 import { WalletBnbAssetsComponent } from './wallet-bnb-assets/wallet-bnb-assets.component'
 import { ClipboardModule } from 'ngx-clipboard'
@@ -47,6 +48,7 @@ import { ToastrModule } from 'ngx-toastr'
     DashboardComponent,
     WalletBnbComponent,
     WalletBnbAssetsComponent,
+    KeystoreTxComponent,
   ],
   imports: [
     AppRoutingModule,
---
+++ src/app/keystore-tx/keystore-tx.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'keystore-tx',
+  templateUrl: './keystore-tx.component.html'
+})
+export class KeystoreTxComponent {
+}
