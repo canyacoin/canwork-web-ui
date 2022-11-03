8dc8a9239658621bfbf9c708c76b661da417e379
GECKO <4787777-geckojs@users.noreply.gitlab.com>
2022-11-03 14:55:05 +0100

    implement listen chain https function to process external events and save into bep20 monitor table, disable monitor entries creation from fronted

--- functions/src/listen-chain.ts
+++ functions/src/listen-chain.ts
@@ -1,14 +1,35 @@
-import axios from 'axios'
 import { GenerateGuid } from './generate.uid'
+const SUPPORTED_METHODS = ['depositBEP20'];
+
 
 /*
 how to test this function:
 https://firebase.google.com/docs/functions/local-shell#invoke_https_functions
   send POST request with form data and authorization 
-  listenToChainUpdates({method:'post',url:'/',headers:{authorization:'123'}}).form( {foo: 'bar' })
+  listenToChainUpdates({method:'post',url:'/',headers:{authorization:env.chainmonitor.authkey}}).form( {method: '..', data: {}, .. })
  */
 
 export async function listenToChainUpdates(request, response, db, env) {
+  /*
+  expected method POST
+  expected path /
+  expected body:
+  {
+    hash, (tx hash)
+    from, (sender address)
+    address, (escrow address)
+    method, (smart contract method) supported: ['depositBEP20']
+    data (smart contract data):
+    { // example for depositBEP20 method
+      asset: token address,
+      provider: provider address,
+      value: big int amount,
+      JOBID: big int job id ,
+      swapPath: address array
+    }    
+  }
+  + headers.authorization
+  */  
   if (request.method !== 'POST') {
     return response
       .status(405)
@@ -21,7 +42,6 @@ export async function listenToChainUpdates(request, response, db, env) {
       .type('application/json')
       .send({ message: 'Path Not Allowed' })
   }
-  console.log(request.headers); // debug
   if (
     !request.headers.authorization ||
     !env.chainmonitor ||
@@ -31,10 +51,148 @@ export async function listenToChainUpdates(request, response, db, env) {
     return response.status(403).send('Unauthorized')
   }  
 
-  console.log(request.body);
+  const body = request.body;
+  // check mandatory params
+  if (
+    !body.hasOwnProperty('hash') ||
+    !body.hasOwnProperty('from') ||
+    !body.hasOwnProperty('address') ||
+    !body.hasOwnProperty('hash') ||
+    !body.hasOwnProperty('method') ||
+    !body.hasOwnProperty('data')      
+  ) {
+    console.log('Missing params');
+    console.log(body);
   
+    return response.status(405).send('Invalid')
+    
+  }
+  
+  const smartMethod = body.method;
+  
+  // now check parameter method specific and method
+  if (SUPPORTED_METHODS.indexOf(smartMethod) === -1) {
+    console.log('Invalid method');
+    console.log(body);
+  
+    return response.status(405).send('Not valid')
+    
+  }
+  
+  // now check method and handle specific mandatory params and actions
+  const smartData = body.data;
+  
+  if (smartMethod === 'depositBEP20') {
+    // deposit to escrow from client
+  
+    if (
+      !smartData.hasOwnProperty('asset') ||
+      !smartData.hasOwnProperty('provider') ||
+      !smartData.hasOwnProperty('value') ||
+      !smartData.hasOwnProperty('JOBID') ||
+      !smartData.hasOwnProperty('swapPath')      
+    ) {
+      console.log('Missing smart contract data');
+      console.log(smartData);
+    
+      return response.status(405).send('Missing data')
+      
+    }
+
+
+    // evertyhing is good, create record
+    const monitorCollection = db.collection('bep20-txs');
+    
+    
+    // generate back uuid from bigint jobId (reverse generate.uid)
+    
+    const jobIdBigInt = smartData.JOBID;
+    
+    const jobIdHex = bigint2hex(jobIdBigInt);
+    
+    // add back stripped minus    
+    const jobId = `${jobIdHex.substr(0,8)}-${jobIdHex.substr(8,4)}-${jobIdHex.substr(12,4)}-${jobIdHex.substr(16,4)}-${jobIdHex.substr(20,12)}`;
+    
+    const amount = smartData.value; // big int string
+    const escrowAddress = body.address; // the to of transaction (interacted address)
+    const tokenAddress = smartData.asset; // the asset sent to escrow
+    const providerAddress = smartData.provider; // the provider address
+    const token = 'token'; // todo: import env from angular and map address to token name
+    const hash = body.hash; // transaction hash
+    const from = body.from;
+    const to = body.address; // transaction to
+    const action = 'deposit';
+    
+    const transaction = {
+      id: GenerateGuid(),
+      timestamp: Date.now(),
+      jobId,
+      // userId,  // (firestore id) this is not mandatory for bep20-monitor
+      // providerId, // (firestore id) this is not mandatory for bep20-monitor
+      amount,
+      escrowAddress,
+      token,
+      tokenAddress,
+      providerAddress,
+      hash,
+      from,
+      to,
+      action,
+      status: 'created' // later it will become 'processed' or 'checked' (if already valid) or 'error'
+      //backend will add processedTimestamp
+    }    
+    
+    /* 
+    only logged in userId can create this tx, this is security rule:
+    
+    // bep20 monitor
+    match /bep20-txs/{transactionId} {
+       allow read: if isAdmin();
+       allow create: if request.auth.uid == request.resource.data.userId;
+       allow update: if isAdmin();
+       allow delete: if isAdmin();
+    }
+          
+    TODO ADD ALSO POSSIBILITY FOR ADMIN (firestore function) TO CREATE THIS RECORD
+    */
+    monitorCollection.doc(transaction.id).set(transaction);
+    
+    
+    // todo then invoke processing function (todo refactor from bep20-monitor and make it
+    // a shared function, to process it instantly
+    // otherwise it will caught up from periodic schedule
+    
+  
+  } else {
+    console.log('Method not implemented');
+    console.log(body);
+    return response.status(405).send('Not implemented')
+    
+  }
+
+  // final response
   return response
     .status(200)
     .type('application/json')
     .send({ status: 'ok' })
+}
+
+/* helpers */
+function bigint2hex(str){ // .toString(16) only works up to 2^53
+    const dec = str.toString().split('');
+    const sum = [];
+    const hex = [];
+    let i, s;
+    while(dec.length){
+        s = 1 * dec.shift()
+        for(i = 0; s || i < sum.length; i++){
+            s += (sum[i] || 0) * 10
+            sum[i] = s % 16
+            s = (s - sum[i]) / 16
+        }
+    }
+    while(sum.length){
+        hex.push(sum.pop().toString(16))
+    }
+    return hex.join('')
 }
