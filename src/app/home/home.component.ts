081054bca9b77cf57748033f7e6534f77042cde7
Samus <10015934-SamusElderg@users.noreply.gitlab.com>
2023-01-25 17:19:17 +0800

    hero and providers sections
    - built structure of the hero & freelancer categories sections
    - applied design-styles and created responsive styles for hero & freelancer categories sections
    - modified and uploaded the new provider icons (drop shadow ripped out of the SVGs and handled with CSS instead)
    - extended the `providerTypes` object to include the new icons and a `desc` prop
    - added an arrow-right svg
    - added a `built-bnb-chain` png
    - modified and added the hero image (world globe image no longer clipped)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -37,29 +37,47 @@ export class HomeComponent implements OnInit {
     private userService: UserService
   ) {}
   providerTypes = [
+    // TODO: Update the copy content for descriptions (the design didnt have unique content for this)
     {
       name: 'Content Creators',
-      img: 'writer.svg',
+      img: 'content-creators.svg',
+      desc:
+        'Hire professional freelance writers who are ready to create quality content for your business at an affordable price. Our freelancers are vetted and  have been creating high-quality content for customers both small and large for years.',
       id: 'contentCreator',
     },
     {
       name: 'Software Developers',
-      img: 'dev.svg',
+      img: 'software-developers.svg',
+      desc:
+        'Looking for experienced developers to work with you? CanWork features only the best of professional developers who have a passion for the craft. You will be able to work with various developers on including front end, back end and full stack development.',
       id: 'softwareDev',
     },
     {
       name: 'Designers & Creatives',
-      img: 'creatives.svg',
+      img: 'designers-creatives.svg',
+      desc:
+        'Find the right freelance designer for your next project. Battle tested and combat ready designers with great years of experience. Our creative professionals have a great range of skillsets, including UI/UX design, graphic design, brand identity and product design.',
       id: 'designer',
     },
     {
+      name: 'Financial Experts',
+      img: 'financial-experts.svg',
+      desc:
+        'Wall Street’s best minds available here on CanWork to help you get your finances in order and make better decisions. Have greater access to capital and make more money by working with the best freelancers on CanWork.',
+      id: 'financial',
+    },
+    {
       name: 'Marketing & Seo',
-      img: 'marketing.svg',
+      img: 'marketing-seo.svg',
+      desc:
+        'As a content creator for hire, our professional team of writers is ready to create quality content for your business at an affordable price. We have been creating high-quality educational content for customers both small and large for years.',
       id: 'marketing',
     },
     {
       name: 'Virtual Assistants',
-      img: 'assistant.svg',
+      img: 'virtual-assistants.svg',
+      desc:
+        'As a content creator for hire, our professional team of writers is ready to create quality content for your business at an affordable price. We have been creating high-quality educational content for customers both small and large for years',
       id: 'virtualAssistant',
     },
   ]
