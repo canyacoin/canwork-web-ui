e1b4647b415d0b24e1407ffbbd26fd114b555485
GECKO <4787777-geckojs@users.noreply.gitlab.com>
2024-01-05 10:43:05 +0000

    replace outdated ngx-simple-modal with ngx-modalview compatible with angular 16 and update module and service use

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { SimpleModalModule } from 'ngx-simple-modal'
+
+//import { SimpleModalModule } from 'ngx-simple-modal' // old module
+import { NgxModalView } from 'ngx-modalview'
+
 import { StarRatingModule } from 'angular-star-rating'
 import { OrderModule } from 'ngx-order-pipe'
 import { NgxPaginationModule } from 'ngx-pagination'
@@ -28,7 +31,8 @@ import { BscPaymentSelectorComponent } from '../core-components/bsc-payment-sele
 @NgModule({
   imports: [
     CommonModule,
-    SimpleModalModule,
+    //SimpleModalModule, // old
+    NgxModalView, // new
     CoreComponentsModule,
     CoreServicesModule,
     CoreUtilsModule,
