816ae6d750ba75ad1658ecf0f5e8265a00863f88
GECKO <4787777-geckojs@users.noreply.gitlab.com>
2024-03-22 11:55:59 +0000

    implement paging into results component and search module, sync address bar and algolia query

--- src/app/search/result/result.component.ts
+++ src/app/search/result/result.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core'
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core'
 
 interface PageEvent {
   first: number
@@ -15,16 +15,22 @@ export class ResultComponent implements OnInit {
   @Input() profileCards: any[]
   @Input() isLoading: boolean = true
   @Input() noSearchParams: boolean = true
+  @Input() totalRecords: number = 0
+  @Input() rows: number = 10
+  @Output() pageChange = new EventEmitter<number>() // two way binding to parent
 
+  /*
+  https://www.primefaces.org/primeng-v14-lts/paginator
+  */
   skCards = new Array(12)
   isGrid: boolean = true
 
   first: number = 0
-  rows: number = 10
 
-  onPageChange(event: PageEvent) {
-    this.first = event.first
-    this.rows = event.rows
+  onPageChange(e: PageEvent) {
+    this.first = e.first
+    //this.rows = event.rows // this is injected from parent
+    this.pageChange.emit(e.page) // notify parent and algolia handler
   }
 
   handleGridChange(isGridView: boolean) {
