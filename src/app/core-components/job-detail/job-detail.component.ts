48d17ef9d6b09f31a1763add231260eded1f169a
Massimo Burlando <massimo951220@gmail.com>
2024-06-10 10:40:37 +0200

    Job Detail

--- src/app/core-components/job-detail/job-detail.component.ts
+++ src/app/core-components/job-detail/job-detail.component.ts
@@ -1,9 +1,8 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'job-detail-panel',
   templateUrl: './job-detail.component.html',
-  styleUrls: ['./job-detail.component.css']
 })
 export class JobDetailComponent {
   @Input() job: any
