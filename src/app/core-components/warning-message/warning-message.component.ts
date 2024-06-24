7f6942a35b8322bc0345c2f9c93910c72ac4b38e
Massimo Burlando <massimo951220@gmail.com>
2024-06-24 23:58:09 +0200

    warning-message updated

--- src/app/core-components/warning-message/warning-message.component.ts
+++ src/app/core-components/warning-message/warning-message.component.ts
@@ -1,15 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'warning-message',
   templateUrl: './warning-message.component.html',
 })
 export class WarningMessageComponent {
-  @Input() message!: string
+  @Input() message: string
   @Input() type: number
+  @Input() budget: number = 0
+
+  @Output() buttonClick = new EventEmitter() // Use specific type instead of any if possible
+
   show: boolean = true
 
   changeViewOption() {
     this.show =!this.show
+  }
+
+  handleClick(event: Event): void {
+    event.preventDefault()
+    this.buttonClick.emit()
   }
 }
