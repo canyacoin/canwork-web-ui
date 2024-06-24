4c20d26b4a113cd018d99be12c033cfcf65f18b0
Massimo Burlando <massimo951220@gmail.com>
2024-06-24 23:58:31 +0200

    basic-tag updated

---
+++ src/app/core-components/tags/basic-tag/basic-tag.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core'
+
+@Component({
+  selector: 'basic-tag',
+  templateUrl: './basic-tag.component.html',
+})
+export class BasicTagComponent {
+  @Input() type!: number
+  @Input() title!: string
+
+  get ButtonStyle() {
+    let style = ''
+
+    if (this.type === 1)
+      style +=
+        'bg-white hover:bg-G200 active:bg-G300 text-G700 border border-G300 hover:border-G200 active:border-G300'
+    else if (this.type === 2)
+      style +=
+        'bg-blue-10 hover:bg-blue-50 active:bg-blue-500 text-C800 border-none'
+    else if (this.type === 3)
+      style += 'bg-blue-50 hover:bg-blue-70 text-white border-none'
+
+    return style
+  }
+}
