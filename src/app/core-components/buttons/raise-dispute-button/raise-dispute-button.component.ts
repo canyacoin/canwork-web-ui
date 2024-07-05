90666fb378e67461580e87f4da6106c3a5152bfa
Massimo Burlando <massimo951220@gmail.com>
2024-07-05 03:44:40 +0200

    raise-dispute-button added

---
+++ src/app/core-components/buttons/raise-dispute-button/raise-dispute-button.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'raise-dispute-button',
+  templateUrl: './raise-dispute-button.component.html',
+})
+export class RaiseDisputeButtonComponent {}
