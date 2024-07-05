dd9b52ce487022e985ae825cf87fc5e79fcd548c
Massimo Burlando <massimo951220@gmail.com>
2024-07-06 01:04:56 +0200

    job-freelancer-information-panel updated

--- src/app/core-components/job/job-freelancer-information-panel/job-freelancer-information-panel.component.ts
+++ src/app/core-components/job/job-freelancer-information-panel/job-freelancer-information-panel.component.ts
@@ -1,15 +1,58 @@
-import { Component, Input } from '@angular/core'
+import {
+  Component,
+  Input,
+  ViewChild,
+  ElementRef,
+  Renderer2,
+  AfterViewInit,
+} from '@angular/core'
 import { Bid } from '@class/job'
+
+import { trigger, state, style, transition, animate } from '@angular/animations'
 
 @Component({
   selector: 'job-freelancer-information-panel',
   templateUrl: './job-freelancer-information-panel.component.html',
+  animations: [
+    trigger('toggleHeight', [
+      state(
+        'collapsed',
+        style({
+          height: '260px',
+          overflow: 'hidden',
+        })
+      ),
+      state(
+        'expanded',
+        style({
+          height: '*',
+        })
+      ),
+      transition('collapsed <=> expanded', [animate('150ms')]),
+    ]),
+  ],
 })
-export class JobFreelancerInformationPanelComponent {
+export class JobFreelancerInformationPanelComponent implements AfterViewInit {
+  @ViewChild('contentDiv') contentDiv: ElementRef
   @Input() selectedBid!: Bid
 
+  isSeeMore: boolean = false
+  isHeightMoreThan259px: boolean = false
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    this.checkHeight()
+  }
+
+  checkHeight() {
+    const height = this.contentDiv.nativeElement.offsetHeight
+    this.isHeightMoreThan259px = height > 259
+  }
+
+
   ngOnInit() {
-    console.log('=======================================')
-    console.log('this.selectedBid = ' + this.selectedBid)
+    // console.log('=======================================')
+    // console.log('this.selectedBid = ' + this.selectedBid)
   }
 }
