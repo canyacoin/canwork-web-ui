63b128e8d6c8e6598f6e3b071d6fbc7a6bcb951e
Massimo Burlando <massimo951220@gmail.com>
2024-07-10 03:21:26 +0200

    back-to-edit button updated

---
+++ src/app/core-components/buttons/back-to-edit-button/back-to-edit-button.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'back-to-edit-button',
+  templateUrl: './back-to-edit-button.component.html',
+})
+export class BackToEditButtonComponent {}
