4fde1100f7b137eb95d52af571d1dae4171615b8
Massimo Burlando <massimo951220@gmail.com>
2024-07-11 22:23:01 +0200

    action-dialog component updated, connect-wallet, public-job updated

--- src/app/core-components/dialogs/action-dialog/action-dialog.component.ts
+++ src/app/core-components/dialogs/action-dialog/action-dialog.component.ts
@@ -12,11 +12,15 @@ import {
   UntypedFormGroup,
   Validators,
 } from '@angular/forms'
-import { Job, PaymentType } from '@class/job'
+import { Job, PaymentType, JobState, Bid } from '@class/job'
 import { ActionType, IJobAction } from '@class/job-action'
-import { UserType } from '@class/user'
+import { User, UserType } from '@class/user'
 import { JobService } from '@service/job.service'
 import { getUsdToCan } from '@util/currency-conversion'
+import { BscService } from '@service/bsc.service'
+import { Router } from '@angular/router'
+import { MessageService } from 'primeng/api'
+import { UserService } from '@service/user.service'
 
 @Component({
   selector: 'action-dialog',
@@ -39,8 +43,9 @@ export class ActionDialogComponent implements OnInit, OnChanges {
   @Input() userType: UserType
 
   @Input() actionType: ActionType
+  @Input() selectedBid: Bid
 
-  otherParty: string = ''
+  otherParty: string = 'the other party'
   action: IJobAction
 
   executing = false
@@ -49,18 +54,67 @@ export class ActionDialogComponent implements OnInit, OnChanges {
   form: UntypedFormGroup = this.formBuilder.group({}) // Initialize with an empty form group
 
   actionTypes = ActionType
+  isReleasing = false
+
+  numberOfStars = 0
+  commentText = ''
+  jobPoster: User = null
 
   constructor(
     private formBuilder: UntypedFormBuilder,
-    private jobService: JobService
+    private jobService: JobService,
+    private bscService: BscService,
+    private router: Router,
+    private messageService: MessageService,
+    private userService: UserService
   ) {}
 
-  ngOnInit(): void {
-    this.otherParty = this.job?.otherParty?.name || 'the other party'
+  async ngOnInit() {
+    console.log('=================>', this.job)
+    if (this.job)
+      this.otherParty = this.job?.otherParty?.name || 'the other party'
+    if (this.job) {
+      await this.setClient(this.job.clientId)
+    }
+  }
+
+  async setClient(clientId: string) {
+    /*
+    new one, retrieve user only once (if not already retrieved)
+    and use the new fastest Algolia getUserById service version
+    */
+    console.log('this.jobPoster', this.jobPoster)
+
+    if (!this.jobPoster) {
+      this.jobPoster = await this.userService.getUser(clientId)
+      console.log('this.jobPoster', this.jobPoster)
+
+      if (this.jobPoster) {
+        let avatar = this.jobPoster.avatar // current, retrocomp
+        //console.log(result[i])
+        if (
+          this.jobPoster.compressedAvatarUrl &&
+          this.jobPoster.compressedAvatarUrl != 'new'
+        ) {
+          // keep same object structure
+          // use compress thumbed if exist and not a massive update (new)
+          avatar = {
+            uri: this.jobPoster.compressedAvatarUrl,
+          }
+        }
+        this.jobPoster.avatarUri = avatar.uri
+      }
+    }
+  }
+
+  setStars(value: number) {
+    this.numberOfStars = value
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    if (changes.visible && changes.visible.currentValue === true) {
+    if (changes.visible && this.visible === true) {
+      this.numberOfStars = 0
+      this.commentText = ''
       this.initializeForm()
     }
   }
@@ -105,15 +159,15 @@ export class ActionDialogComponent implements OnInit, OnChanges {
         //   message: ['', Validators.required],
         // })
         break
-      case ActionType.review:
-        this.form = this.formBuilder.group({
-          message: [
-            '',
-            Validators.compose([Validators.min(0), Validators.max(350)]),
-          ],
-          rating: [null, Validators.required],
-        })
-        break
+      // case ActionType.review:
+      //   this.form = this.formBuilder.group({
+      //     message: [
+      //       '',
+      //       Validators.compose([Validators.min(0), Validators.max(350)]),
+      //     ],
+      //     rating: [null, Validators.required],
+      //   })
+      //   break
       case ActionType.acceptTerms:
         this.form = this.formBuilder.group({
           terms: [false, Validators.requiredTrue],
@@ -146,8 +200,10 @@ export class ActionDialogComponent implements OnInit, OnChanges {
           )
           break
         case ActionType.review:
-          this.action.message = this.form.value.message
-          this.action.rating = this.form.value.rating
+          // this.action.message = this.form.value.message
+          // this.action.rating = this.form.value.rating
+          this.action.message = this.commentText
+          this.action.rating = this.numberOfStars
           break
         case ActionType.addMessage:
           this.action.message = this.form.value.message
@@ -193,4 +249,57 @@ export class ActionDialogComponent implements OnInit, OnChanges {
   // usdToCan(usd: number) {
   //   return getUsdToCan(this.usdToAtomicCan, usd)
   // }
+
+  async releaseBtnClick(event: Event) {
+    event.preventDefault()
+    if (this.job.bscEscrow === true) {
+      console.log('ActionType.acceptFinish BEP20')
+      this.isReleasing = true
+      await this.releaseEscrowBsc()
+      this.visible = false
+    }
+  }
+
+  private async releaseEscrowBsc() {
+    console.log('release Escrow BSC')
+    // we check if bsc chain is connected and if not, suggest to connect to bsc chain explicitely (for now only metamask, not bep2 chain
+    if (!(await this.bscService.isBscConnected())) {
+      const routerStateSnapshot = this.router.routerState.snapshot
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Warn',
+        detail: `Connect your BNBChain wallet to release the payment`,
+      })
+
+      this.router.navigate(['/wallet-bnb'], {
+        queryParams: { returnUrl: routerStateSnapshot.url },
+      })
+      return
+    }
+
+    // we are bsc connected, go on
+    const jobId = this.job.id
+    console.log('releasing jobId ' + jobId)
+    let result = await this.bscService.release(jobId)
+    if (!result.err) {
+      // save tx immediately
+      this.isReleasing = false
+
+      // moved to backend
+      /*
+      let tx = await this.transactionService.createTransaction(
+        `Release funds`,
+        result.transactionHash,
+        jobId
+      )*/
+
+      const action = new IJobAction(ActionType.acceptFinish, UserType.client)
+      this.job.state = JobState.complete
+      await this.jobService.handleJobAction(this.job, action)
+    }
+  }
+
+  get isClient(): boolean {
+    return this.userType === UserType.client
+  }
 }
