e3828f0eb7b24428f20eacedd42adb15e43cb7d7
Massimo Burlando <massimo951220@gmail.com>
2024-07-11 22:21:39 +0200

    verified-mark updated

--- src/app/core-components/verified-mark/verified-mark.component.ts
+++ src/app/core-components/verified-mark/verified-mark.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, OnInit, Directive } from '@angular/core'
+import { User } from '@class/user'
 
 @Component({
   selector: 'verified-mark',
   templateUrl: './verified-mark.component.html',
 })
 export class VerifiedMarkComponent implements OnInit {
-  @Input() user: any
+  @Input() user: User
 
   constructor() {}
 
