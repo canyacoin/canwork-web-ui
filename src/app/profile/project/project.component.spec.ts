7b9d18def2e03fc2b0a2e568a21e0fbffddedb90
Massimo Burlando <massimo951220@gmail.com>
2024-07-29 00:47:37 +0200

    project component added

---
+++ src/app/profile/project/project.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+
+import { ProjectComponent } from './project.component'
+
+describe('ProjectComponent', () => {
+  let component: ProjectComponent
+  let fixture: ComponentFixture<ProjectComponent>
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ProjectComponent],
+    }).compileComponents()
+  }))
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProjectComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should create', () => {
+    expect(component).toBeTruthy()
+  })
+})
