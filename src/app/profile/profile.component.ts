076e3d21ea9559e1c622a3cf084d16e378db3c5a
Massimo Burlando <massimo951220@gmail.com>
2024-07-29 00:47:51 +0200

    profile page updated

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,207 @@
+import { Location } from '@angular/common'
+import { Component, Input, OnDestroy, OnInit, Directive } from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router'
+import { User } from '@class/user'
+import { AuthService } from '@service/auth.service'
+import { UserService } from '@service/user.service'
+import { ToastrService } from 'ngx-toastr'
+import { PublicJobService } from '@service/public-job.service'
+import { SeoService } from '@service/seo.service'
+import { Subscription } from 'rxjs'
+import { take } from 'rxjs/operators'
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss'],
+})
+export class ProfileComponent implements OnInit, OnDestroy {
+  currentUser: User
+  currentUserJobs: any
+
+  userModel: User
+  authSub: Subscription
+
+  paramsSub: Subscription
+
+  displayEditComponent = false
+  notifiedBnbOrBscAddress = false
+
+  navigationAddress = null
+
+  constructor(
+    private router: Router,
+    private location: Location,
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService,
+    private authService: AuthService,
+    private publicJobService: PublicJobService,
+    private toastr: ToastrService,
+    private seoService: SeoService
+  ) {
+    const navigation = this.router.getCurrentNavigation()
+    this.navigationAddress = navigation.extras.state
+      ? navigation.extras.state.id
+      : null
+  }
+
+  ngOnInit() {
+    this.authSub = this.authService.currentUser$.subscribe(
+      (user: User) => {
+        if (user !== this.currentUser) {
+          this.currentUser = user
+
+          this.activatedRoute.params.pipe(take(1)).subscribe((params) => {
+            this.initUsers(this.currentUser, params)
+          })
+          this.activatedRoute.queryParams.subscribe((params) => {
+            const redirected = this.redirectToUniqueUrlIfNecessary(params)
+            if (!redirected) {
+              this.notifyAddAddressIfNecessary(user)
+            }
+
+            this.displayEditComponent = params.editProfile ? true : false
+          })
+        }
+      },
+      (error) => {
+        console.error('! unable to retrieve currentUser data:', error)
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    if (this.paramsSub) {
+      this.paramsSub.unsubscribe()
+    }
+    if (this.authSub) {
+      this.authSub.unsubscribe()
+    }
+  }
+
+  async notifyAddAddressIfNecessary(loggedInUser) {
+    if (!loggedInUser) return
+    if (this.notifiedBnbOrBscAddress) {
+      return
+    }
+    const noAddress = !!loggedInUser && !loggedInUser.bscAddress
+    if (noAddress && loggedInUser.type == 'Provider') {
+      this.toastr.warning('Add BNB Chain (BEP20) wallet to receive payments')
+      this.notifiedBnbOrBscAddress = true
+    }
+  }
+
+  initUsers(user: User, params: any) {
+    const { address: originalAddress, slug } = params
+    let address = originalAddress
+    // we have address, use it so we can retrieve object by id from algolia
+    if (!originalAddress && this.navigationAddress)
+      address = this.navigationAddress
+
+    if (
+      (address && this.currentUser && this.currentUser.address === address) ||
+      (slug && this.currentUser && this.currentUser.slug === slug)
+    ) {
+      // optimize, use current user data if logged in
+      console.log('initUsers preload current')
+      this.userModel = this.currentUser
+      this.setUsersColors(this.userModel)
+      return
+    }
+
+    if (address && address !== 'setup') {
+      this.loadUser({ address })
+    } else if (slug) {
+      this.userService.getUserBySlug(slug).then((user) => {
+        if (user) {
+          this.userModel = user
+
+          this.seoService.updateAllSeoProperties(
+            'profile',
+            this.userModel.name,
+            this.userModel.bio,
+            this.userModel.slug,
+            this.userModel.avatar,
+            this.userModel.compressedAvatarUrl
+          )
+        }
+      })
+    } else if (user) {
+      this.userModel = this.currentUser
+      this.setUsersColors(this.userModel)
+    }
+  }
+
+  loadUser(params: any) {
+    const address = params['address']
+    this.userService
+      .getUserById(address)
+      .then((user: User) => {
+        console.log('user profile===========================>', user)
+        this.userModel = user
+        this.redirectToUniqueUrlIfNecessary(params)
+        this.setUsersColors(this.userModel)
+        this.saveWhoViewProfile()
+        this.addToViewedProfileList()
+      })
+      .catch((err) => {
+        console.log(`loadUser ${address} error: ${err.toString()}`)
+        console.log(err)
+      })
+  }
+
+  closeEditDialog() {
+    this.displayEditComponent = false
+    this.router.navigate(['.'], {
+      relativeTo: this.activatedRoute,
+      queryParams: {},
+    })
+  }
+
+  setUsersColors(user: User) {
+    if (user && user.colors.length <= 0) {
+      user.colors = ['#00FFCC', '#33ccff', '#15EDD8']
+    }
+  }
+
+  saveWhoViewProfile() {
+    if (this.notMyProfile() && this.currentUser) {
+      this.userService.saveProfileView(this.currentUser, this.userModel.address)
+    }
+  }
+
+  addToViewedProfileList() {
+    if (this.notMyProfile() && this.currentUser) {
+      this.userService.addToViewedUsers(
+        this.currentUser.address,
+        this.userModel
+      )
+    }
+  }
+
+  isMyProfile() {
+    if (this.currentUser == null) {
+      return false
+    }
+    return this.userModel && this.userModel.address === this.currentUser.address
+  }
+
+  notMyProfile() {
+    if (!this.currentUser && this.userModel) {
+      return true
+    }
+    return this.userModel && this.userModel.address !== this.currentUser.address
+  }
+
+  redirectToUniqueUrlIfNecessary(params: any) {
+    const { address } = params
+    if (this.isMyProfile() && this.router.url.endsWith('/profile')) {
+      this.router.navigate(['/profile', this.currentUser.slug])
+      return true
+    } else if (this.userModel && address && address !== 'setup') {
+      this.router.navigate(['/profile', this.userModel.slug])
+      return true
+    }
+    return false
+  }
+}
---
+++ src/app/profile/profile.routing.module.ts
@@ -0,0 +1,63 @@
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+
+import { AuthGuard } from '../core-utils/auth.guard'
+import { UserIsNotSetupGuard } from '../core-utils/user-is-not-setup.guard'
+import { UserIsSetupGuard } from '../core-utils/user-is-setup.guard'
+import { BuyCoffeeComponent } from './buy-coffee/buy-coffee.component'
+import { EditComponent } from './edit/edit.component'
+import { ProfileViewsComponent } from './profile-views/profile-views.component'
+import { ProfileComponent } from './profile.component'
+import { ProjectComponent } from './project/project.component'
+import { SetupComponent } from './setup/setup.component'
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProfileComponent,
+    canActivate: [UserIsSetupGuard],
+  },
+  {
+    path: 'setup',
+    component: SetupComponent,
+    canActivate: [AuthGuard, UserIsNotSetupGuard],
+    data: { requiresLoggedIn: true },
+  },
+  {
+    path: 'project',
+    component: ProjectComponent,
+    canActivate: [AuthGuard, UserIsSetupGuard],
+    data: { requiresLoggedIn: true },
+  },
+  {
+    path: 'project/:id',
+    component: ProjectComponent,
+    canActivate: [AuthGuard, UserIsSetupGuard],
+    data: { requiresLoggedIn: true },
+  },
+  {
+    path: 'views',
+    component: ProfileViewsComponent,
+    canActivate: [AuthGuard, UserIsSetupGuard],
+    data: { requiresLoggedIn: true },
+  },
+  {
+    path: 'alt/:address',
+    component: ProfileComponent,
+  },
+  {
+    path: 'buy-coffee/:address',
+    component: BuyCoffeeComponent,
+  },
+  {
+    path: ':slug',
+    component: ProfileComponent,
+  },
+]
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ProfileRoutingModule {}
