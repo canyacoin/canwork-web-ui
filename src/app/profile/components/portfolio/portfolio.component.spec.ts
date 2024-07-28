639939e4f82947258323be86a355fb0581778365
Massimo Burlando <massimo951220@gmail.com>
2024-07-29 00:49:24 +0200

    portfolio added

---
+++ src/app/profile/components/portfolio/portfolio.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+
+import { PortfolioComponent } from './portfolio.component'
+
+describe('PortfolioComponent', () => {
+  let component: PortfolioComponent
+  let fixture: ComponentFixture<PortfolioComponent>
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [PortfolioComponent],
+    }).compileComponents()
+  }))
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PortfolioComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should create', () => {
+    expect(component).toBeTruthy()
+  })
+})
