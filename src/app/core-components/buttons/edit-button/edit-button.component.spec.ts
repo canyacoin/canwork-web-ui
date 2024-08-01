6b9543a22d536e8f1378817b7394d1d3aa028a86
Massimo Burlando <massimo951220@gmail.com>
2024-08-02 00:39:07 +0200

    edit-button added

---
+++ src/app/core-components/buttons/edit-button/edit-button.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditButtonComponent } from './edit-button.component';
+
+describe('EditButtonComponent', () => {
+  let component: EditButtonComponent;
+  let fixture: ComponentFixture<EditButtonComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [EditButtonComponent]
+    });
+    fixture = TestBed.createComponent(EditButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
