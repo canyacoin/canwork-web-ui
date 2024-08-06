2d4d2e0f7a483ad5ad7f3e7adef08e1a5fd5d7e5
Massimo Burlando <massimo951220@gmail.com>
2024-08-06 02:22:28 +0200

    three-dots-button added

---
+++ src/app/core-components/buttons/three-dots-button/three-dots-button.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'three-dots-button',
+  templateUrl: './three-dots-button.component.html',
+})
+export class ThreeDotsButtonComponent {}
