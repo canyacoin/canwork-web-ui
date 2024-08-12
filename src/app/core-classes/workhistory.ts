8f528c96209b3809cfc00dc61428a03a76a8e294
Massimo Burlando <massimo951220@gmail.com>
2024-08-12 04:22:05 +0200

    Workhistory class added

---
+++ src/app/core-classes/workhistory.ts
@@ -0,0 +1,10 @@
+export class Workhistory {
+  id: string
+  logoUrl: string
+  title: string
+  employer: string
+  startDate: number
+  completion: number
+  description: string
+  tags: string[] = []
+}
