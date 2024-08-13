1cd554dd55f17b6dd5ff24002ed72ceccd481fab
Massimo Burlando <massimo951220@gmail.com>
2024-08-13 03:37:09 +0200

    sk-workhistory updated

---
+++ src/app/core-components/skeletons/sk-workhistory/sk-workhistory.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'sk-workhistory',
+  templateUrl: './sk-workhistory.component.html',
+})
+export class SkWorkhistoryComponent {}
