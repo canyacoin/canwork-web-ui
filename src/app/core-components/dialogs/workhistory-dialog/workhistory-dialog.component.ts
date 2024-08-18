2d1c0f8e3e373f9c9979a8a76c9bbfb7722c772c
Massimo Burlando <massimo951220@gmail.com>
2024-08-18 22:39:57 +0200

    work history part done

--- src/app/core-components/dialogs/workhistory-dialog/workhistory-dialog.component.ts
+++ src/app/core-components/dialogs/workhistory-dialog/workhistory-dialog.component.ts
@@ -9,10 +9,10 @@ import { User } from '@class/user'
 import { Workhistory } from '@class/workhistory'
 import { WorkhistoryService } from '@service/workhistory.service'
 import { AuthService } from '@service/auth.service'
-import { HttpClient } from '@angular/common/http'
 import { UntypedFormBuilder, Validators } from '@angular/forms'
 import { Subscription } from 'rxjs/Subscription'
 import { MessageService } from 'primeng/api'
+import { AngularFireStorage } from '@angular/fire/compat/storage'
 
 @Component({
   selector: 'workhistory-dialog',
@@ -33,6 +33,12 @@ export class WorkhistoryDialogComponent {
 
   @Input() selectedWorkhistory: Workhistory | null = null
 
+  filePath: string
+
+  isUploading: boolean = false
+  selectedFile: File | null = null
+  selectedFileUrl: string | ArrayBuffer
+
   uniInput = ''
   uniList: any
   uniFilteredList: any
@@ -44,12 +50,14 @@ export class WorkhistoryDialogComponent {
   yearList = new Array()
   completionYearList = new Array()
 
+  updatedTags: string[] = []
+
   constructor(
     private messageService: MessageService,
     private auth: AuthService,
     public workhistorys: WorkhistoryService,
     private formBuilder: UntypedFormBuilder,
-    private http: HttpClient
+    private storage: AngularFireStorage
   ) {}
 
   ngOnInit() {
@@ -58,15 +66,7 @@ export class WorkhistoryDialogComponent {
         this.currentUser = user
       }
     })
-    this.workhistoryForm = this.formBuilder.group({
-      logoUrl: ['', Validators.required],
-      title: ['', Validators.required],
-      employer: ['', Validators.required],
-      startDate: ['', Validators.required],
-      completion: ['', Validators.required],
-      description: ['', Validators.required],
-      tags: [[], Validators.required],
-    })
+    this.buildForm()
     const currentYear = new Date().getFullYear()
     const maxCompletionYear = currentYear + 10
     for (let i = 0; i < 60; i++) {
@@ -75,29 +75,59 @@ export class WorkhistoryDialogComponent {
     for (let i = 0; i < 60; i++) {
       this.completionYearList.push(maxCompletionYear - i)
     }
+    if (this.currentUser) {
+      this.filePath = `uploads/workhistorys/${this.currentUser.address}`
+      console.log('this.filePath', this.filePath)
+    }
+  }
+
+  buildForm() {
+    this.updatedTags = []
+    this.workhistoryForm = this.formBuilder.group({
+      logoUrl: [''],
+      title: ['', Validators.required],
+      employer: ['', Validators.required],
+      startDate: ['', Validators.required],
+      completion: ['', Validators.required],
+      description: ['', Validators.required],
+      tags: [[], Validators.required],
+    })
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes.selectedWorkhistory && this.visible === true) {
-      this.workhistoryForm.controls.logoUrl.setValue(
-        this.selectedWorkhistory.logoUrl
-      )
-      this.workhistoryForm.controls.title.setValue(
-        this.selectedWorkhistory.title
-      )
-      this.workhistoryForm.controls.employer.setValue(
-        this.selectedWorkhistory.employer
-      )
-      this.workhistoryForm.controls.startDate.setValue(
-        this.selectedWorkhistory.startDate
-      )
-      this.workhistoryForm.controls.completion.setValue(
-        this.selectedWorkhistory.completion
-      )
-      this.workhistoryForm.controls.description.setValue(
-        this.selectedWorkhistory.description
-      )
-      this.workhistoryForm.controls.tags.setValue(this.selectedWorkhistory.tags)
+      console.log('selectedWorkhistory', this.selectedWorkhistory)
+      if (this.selectedWorkhistory !== null) {
+        this.workhistoryForm.controls.logoUrl.setValue(
+          this.selectedWorkhistory.logoUrl
+        )
+        this.workhistoryForm.controls.title.setValue(
+          this.selectedWorkhistory.title
+        )
+        this.workhistoryForm.controls.employer.setValue(
+          this.selectedWorkhistory.employer
+        )
+        this.workhistoryForm.controls.startDate.setValue(
+          this.selectedWorkhistory.startDate
+        )
+        this.workhistoryForm.controls.completion.setValue(
+          this.selectedWorkhistory.completion
+        )
+        this.workhistoryForm.controls.description.setValue(
+          this.selectedWorkhistory.description
+        )
+        this.workhistoryForm.controls.tags.setValue(
+          this.selectedWorkhistory.tags
+        )
+        this.updatedTags =
+          this.selectedWorkhistory.tags === ''
+            ? []
+            : this.selectedWorkhistory.tags
+                .split(',')
+                .map((item) => item.trim())
+      } else {
+        this.buildForm()
+      }
     }
   }
 
@@ -107,9 +137,9 @@ export class WorkhistoryDialogComponent {
     }
   }
 
-  onSubmitEducation() {
+  async onSubmitWorkhistory() {
     const tempWorkhistory = new Workhistory()
-    tempWorkhistory.logoUrl = this.workhistoryForm.value.logoUrl
+    tempWorkhistory.logoUrl = ''
     tempWorkhistory.title = this.workhistoryForm.value.title
     tempWorkhistory.employer = this.workhistoryForm.value.employer
     tempWorkhistory.startDate = this.workhistoryForm.value.startDate
@@ -117,15 +147,38 @@ export class WorkhistoryDialogComponent {
     tempWorkhistory.description = this.workhistoryForm.value.description
     tempWorkhistory.tags = this.workhistoryForm.value.tags
 
+    if (this.selectedWorkhistory !== null) {
+      tempWorkhistory.id = this.selectedWorkhistory.id
+    } else {
+      tempWorkhistory.id = this.idGenerator()
+    }
+    if (this.selectedFile !== null) {
+      const task = this.storage.upload(
+        this.filePath + '/' + tempWorkhistory.id,
+        this.selectedFile
+      )
+
+      console.log('task:', task)
+
+      // isUploading
+      this.isUploading = true
+      try {
+        const snap = await task.snapshotChanges().toPromise()
+        const url = await snap.ref.getDownloadURL()
+        tempWorkhistory.logoUrl = url
+      } catch (err) {
+      } finally {
+        this.isUploading = false
+      }
+    }
+
     try {
       if (this.selectedWorkhistory !== null) {
-        tempWorkhistory.id = this.selectedWorkhistory.id
         this.workhistorys.updateWorkhistory(
           tempWorkhistory,
           this.currentUser.address
         )
       } else {
-        tempWorkhistory.id = this.idGenerator()
         this.workhistorys.addWorkhistory(
           tempWorkhistory,
           this.currentUser.address
@@ -140,6 +193,11 @@ export class WorkhistoryDialogComponent {
     }
   }
 
+  skillTagsUpdated(value: string) {
+    console.log('value===============================>', value)
+    this.workhistoryForm.controls['tags'].setValue(value)
+  }
+
   idGenerator(): string {
     function s4() {
       return Math.floor((1 + Math.random()) * 0x10000)
@@ -151,9 +209,9 @@ export class WorkhistoryDialogComponent {
 
   getDialogHeader() {
     if (this.selectedWorkhistory === null) {
-      return 'Add Workhistory'
+      return 'Add Work Experience'
     } else {
-      return 'Edit Workhistory'
+      return 'Edit Work Experience'
     }
   }
   onClose() {
@@ -162,7 +220,38 @@ export class WorkhistoryDialogComponent {
 
   onSave(event: Event) {
     event.preventDefault()
-    this.onSubmitEducation()
+    this.onSubmitWorkhistory()
     this.visible = false
+  }
+
+  detectFiles(event: Event) {
+    const input = event.target as HTMLInputElement
+    if (input.files && input.files.length) {
+      const file = input.files[0]
+
+      // Check if the file type is an image
+      const validImageTypes = ['image/jpg', 'image/jpeg', 'image/png']
+      if (!validImageTypes.includes(file.type)) {
+        alert('Please select a valid image file (jpg, jpeg, or png).')
+        return
+      }
+
+      // Check if the file size is under 4MB
+      const maxSizeInMB = 4
+      if (file.size > maxSizeInMB * 1024 * 1024) {
+        alert('The selected file is too large. Please select a file under 4MB.')
+        return
+      }
+
+      // If the file is valid, read it and set the selectedFile property
+      const reader = new FileReader()
+
+      reader.onload = () => {
+        this.selectedFileUrl = reader.result
+        this.selectedFile = file
+      }
+
+      reader.readAsDataURL(file)
+    }
   }
 }
