7cfdb3f28d5b65d2b031c1d166a79e9446b18a80
Massimo Burlando <massimo951220@gmail.com>
2024-08-21 22:09:10 +0200

    update share-button

--- src/app/core-components/buttons/share-button/share-button.component.ts
+++ src/app/core-components/buttons/share-button/share-button.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
+import { MessageService } from 'primeng/api'
 
 interface ItemType {
   name: string
   img: string
-  code: string
+  code: number
 }
 
 @Component({
@@ -11,18 +12,37 @@ interface ItemType {
   templateUrl: './share-button.component.html',
 })
 export class ShareButtonComponent {
+  @Input() link: string = ''
+
   shareLinks: ItemType[] | undefined
   selectedShareLinks: ItemType | undefined
 
+  constructor(private messageService: MessageService) {}
+
   ngOnInit() {
     this.shareLinks = [
-      { name: 'Invite Freelancer', img: 'fi_user-plus.svg', code: '0' },
-      { name: 'Copy Link', img: 'u_link.svg', code: '1' },
-      { name: 'Twitter', img: 'x.svg', code: '2' },
-      { name: 'Facebook', img: 'logos_facebook.svg', code: '3' },
-      { name: 'Linkedin', img: 'devicon_linkedin.svg', code: '4' },
+      // { name: 'Invite Freelancer', img: 'fi_user-plus.svg', code: 0 },
+      { name: 'Copy Link', img: 'u_link.svg', code: 1 },
+      // { name: 'Twitter', img: 'x.svg', code: 2 },
+      // { name: 'Facebook', img: 'logos_facebook.svg', code: 3 },
+      // { name: 'Linkedin', img: 'devicon_linkedin.svg', code: 4 },
     ]
 
     this.selectedShareLinks = this.shareLinks[0]
+  }
+
+  itemClick(code: number) {
+    if (code === 1 && this.link !== '') {
+      // Copy link to clipboard
+      navigator.clipboard.writeText(this.link)
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: 'Link copied to clipboard!',
+      })
+    }
+    this.selectedShareLinks = this.shareLinks.find(
+      (item) => item.code === code
+    )!
   }
 }
