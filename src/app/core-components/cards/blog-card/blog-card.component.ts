04e992239d95d0d2e245efe4263fb6467e6628b0
Massimo Burlando <massimo951220@gmail.com>
2024-09-06 23:06:13 +0200

    Feat: Add blog-card component

--- src/app/core-components/cards/blog-card/blog-card.component.ts
+++ src/app/core-components/cards/blog-card/blog-card.component.ts
@@ -9,7 +9,12 @@ export class BlogCardComponent {
   @Input() slug: string = ''
   @Input() title: string
   @Input() subTitle: string
+  @Input() body: string
   @Input() tags: string[]
   @Input() author: string
   @Input() datePosted: string
+
+  readingTime() {
+    return Math.ceil(this.body.length / 1000)
+  }
 }
