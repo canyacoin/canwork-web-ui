c1deddbcb218ec6f6a2eae8455105c42825827b5
GECKO <4787777-geckojs@users.noreply.gitlab.com>
2024-11-14 10:21:16 +0000

    configure warm instance for ssr function

--- dist-ssr/functions/index.js
+++ dist-ssr/functions/index.js
@@ -1,12 +1,22 @@
 const functions = require('firebase-functions')
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 const universalMain = require(__dirname + '/server/main')
-exports.ssr = functions.https.onRequest(universalMain.app())
+exports.ssr = functions
+  .runWith({
+    // Keep 1 instance warm for this latency-critical function
+    minInstances: 1,
+    /*
+    https://firebase.google.com/docs/functions/manage-functions?gen=1st#reduce_the_number_of_cold_starts
+    
+    If Cloud Functions for Firebase scales your app above your minInstances setting, you'll experience a cold start for each instance above that threshold.
+    Cold starts have the most severe effect on apps with spiky traffic. If your app has spiky traffic and you set a minInstances value high enough that cold starts are reduced on each traffic increase, you'll see significantly reduced latency. For apps with constant traffic, cold starts are not likely to severely affect performance.    
+
+    Note: A minimum number of instances kept running incur billing costs at idle rates.
+    Typically, to keep one idle function instance warm costs less than $6.00 a month.
+    The Firebase CLI provides a cost estimate at deployment time for functions with reserved minimum instances.
+    Refer to Cloud Functions Pricing to calculate costs.
+    
+    todo configure to use only live, for now test on staging
+    */
+  })
+  .https.onRequest(universalMain.app())
