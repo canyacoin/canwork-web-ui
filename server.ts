d7d4310d63cb5786375543c363862b6b9ef95db2
GECKO <4787777-geckojs@users.noreply.gitlab.com>
2024-11-15 11:17:15 +0000

    add compression and cache manager to improve server rendering performance

--- server.ts
+++ server.ts
@@ -71,10 +71,21 @@ import { APP_BASE_HREF } from '@angular/common'
 import { ngExpressEngine } from '@nguniversal/express-engine'
 import * as express from 'express'
 import { AppServerModule } from './src/main.server'
+const compression = require('compression')
+const cacheManager = require('cache-manager')
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
+  const memoryCache = cacheManager.caching({
+    store: 'memory',
+    max: 10,
+    ttl: 60 * 10 /* seconds */,
+  })
+
   const server = express()
+
+  server.use(compression({ level: 8 })) // gzip compression
+
   //const distFolder = join(process.cwd(), 'dist-ssr/can-work/browser')
   const indexHtml = existsSync(join(distFolder, 'index.original.html'))
     ? 'index.original.html'
@@ -108,12 +119,65 @@ export function app(): express.Express {
   })
 
   // All regular routes use the Universal engine
-  server.get('*', (req, res) => {
-    res.render(indexHtml, {
-      req,
-      providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
-    })
-  })
+  server.get(
+    '*',
+    (req, res, next) => {
+      // A middleware to check if cached response exists
+      console.log('Looking for route in cache: ' + req.originalUrl)
+      // try to get the requested url from cache
+      memoryCache
+        .get(req.originalUrl)
+        .then((cachedHtml) => {
+          if (cachedHtml) {
+            console.log(
+              'Page found in cache, rendering from cache : ' + req.originalUrl
+            )
+            // Cached page exists. Send it.
+            res.send(cachedHtml)
+          } else {
+            // Cached page does not exist.
+            // Render a response using the Angular express engine
+            next()
+          }
+        })
+        .catch((error) => {
+          // if we have an error render using angular univesal
+          console.error('memoryCache.get error: ', error)
+          next()
+        })
+    },
+
+    (req, res) => {
+      console.log(
+        `No cache found, rendering using universal engine: ` + req.originalUrl
+      )
+
+      res.render(
+        indexHtml,
+        {
+          req,
+          providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
+        },
+        (err: Error, html: string) => {
+          // After angular universal express engine renders the page
+          // we cache it so the next requests for this route will be
+          // served faster using the middleware defined earlier
+          console.log(
+            'Page rendered for the first time , caching url: ' + req.originalUrl
+          )
+          // Cache the rendered `html` for this request url to
+          // use for subsequent requests
+          // Cache the rendered page and set the cache to be
+          // eviced after 300s (5 minutes)
+          memoryCache
+            .set(req.originalUrl, html, 300)
+            .catch((err) => console.log('Could not cache the request', err))
+
+          res.send(html)
+        }
+      )
+    }
+  )
 
   return server
 }
